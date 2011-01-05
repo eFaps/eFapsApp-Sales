94e7bd8364d0c987c108ef51a07ef85a7ca2fd64
jorge.cueva@moxter.net <jorge.cueva@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2011-01-05 21:24:26 +0000

    - correction method create of quotation in prices.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@6035 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/Quotation_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/Quotation_Base.java
@@ -22,7 +22,9 @@ package org.efaps.esjp.sales.document;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
+import org.efaps.admin.common.SystemConfiguration;
 import org.efaps.admin.datamodel.Status;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
@@ -30,6 +32,7 @@ import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
+import org.efaps.esjp.ci.CIERP;
 import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.sales.Calculator;
 import org.efaps.util.EFapsException;
@@ -72,11 +75,20 @@ public abstract class Quotation_Base
         final String date = _parameter.getParameterValue("date");
         final List<Calculator> calcList = analyseTable(_parameter, null);
         final Long contactid = Instance.get(_parameter.getParameterValue("contact")).getId();
+        // Sales-Configuration
+        final Instance baseCurrInst = SystemConfiguration.get(
+                        UUID.fromString("c9a1cbc3-fd35-4463-80d2-412422a3802f")).getLink("CurrencyBase");
+        final Instance rateCurrInst = Instance.get(CIERP.Currency.getType(),
+                        _parameter.getParameterValue("rateCurrencyId"));
         final Object[] rateObj = getRateObject(_parameter);
+        final BigDecimal rate = ((BigDecimal) rateObj[0]).divide((BigDecimal) rateObj[1], 12,
+                        BigDecimal.ROUND_HALF_UP);
+
         final Insert insert = new Insert(CISales.Quotation);
         insert.add(CISales.Quotation.Contact, contactid);
-        insert.add(CISales.Quotation.CrossTotal, getCrossTotal(calcList));
-        insert.add(CISales.Quotation.NetTotal, getNetTotal(calcList));
+        insert.add(CISales.Quotation.CrossTotal, getCrossTotal(calcList).divide(rate, BigDecimal.ROUND_HALF_UP));
+        insert.add(CISales.Quotation.NetTotal, getNetTotal(calcList).divide(rate, BigDecimal.ROUND_HALF_UP));
+        insert.add(CISales.Quotation.DiscountTotal, BigDecimal.ZERO);
         insert.add(CISales.Quotation.RateNetTotal, getNetTotal(calcList));
         insert.add(CISales.Quotation.RateCrossTotal, getCrossTotal(calcList));
         insert.add(CISales.Quotation.RateDiscountTotal, BigDecimal.ZERO);
@@ -84,9 +96,8 @@ public abstract class Quotation_Base
         insert.add(CISales.Quotation.Salesperson, _parameter.getParameterValue("salesperson"));
         insert.add(CISales.Quotation.Name, _parameter.getParameterValue("name4create"));
         insert.add(CISales.Quotation.Status, Status.find(CISales.QuotationStatus.uuid, "Open").getId());
-        insert.add(CISales.Quotation.DiscountTotal, BigDecimal.ZERO);
-        insert.add(CISales.Quotation.CurrencyId, _parameter.getParameterValue("rateCurrencyId"));
-        insert.add(CISales.Quotation.RateCurrencyId, _parameter.getParameterValue("rateCurrencyId"));
+        insert.add(CISales.Quotation.CurrencyId, baseCurrInst.getId());
+        insert.add(CISales.Quotation.RateCurrencyId, rateCurrInst.getId());
         insert.add(CISales.Quotation.Rate, rateObj);
         insert.add(CISales.Quotation.Note, _parameter.getParameterValue("note"));
         insert.execute();
@@ -108,7 +119,14 @@ public abstract class Quotation_Base
                                    final CreatedDoc _createdDoc)
         throws EFapsException
     {
+     // Sales-Configuration
+        final Instance baseCurrInst = SystemConfiguration.get(
+                        UUID.fromString("c9a1cbc3-fd35-4463-80d2-412422a3802f")).getLink("CurrencyBase");
+        final Instance rateCurrInst = Instance.get(CIERP.Currency.getType(),
+                        _parameter.getParameterValue("rateCurrencyId"));
         final Object[] rateObj = getRateObject(_parameter);
+        final BigDecimal rate = ((BigDecimal) rateObj[0]).divide((BigDecimal) rateObj[1], 12,
+                        BigDecimal.ROUND_HALF_UP);
         Integer i = 0;
         for (final Calculator calc : _calcList) {
             if (!calc.isEmpty()) {
@@ -121,16 +139,21 @@ public abstract class Quotation_Base
                                 _parameter.getParameterValues("productAutoComplete")[i]);
                 posIns.add(CISales.QuotationPosition.Quantity, calc.getQuantityStr());
                 posIns.add(CISales.QuotationPosition.UoM, _parameter.getParameterValues("uoM")[i]);
-                posIns.add(CISales.QuotationPosition.CrossUnitPrice, calc.getCrossUnitPriceStr());
-                posIns.add(CISales.QuotationPosition.NetUnitPrice, calc.getNetUnitPriceStr());
-                posIns.add(CISales.QuotationPosition.CrossPrice, calc.getCrossPriceStr());
-                posIns.add(CISales.QuotationPosition.NetPrice, calc.getNetPriceStr());
+                posIns.add(CISales.QuotationPosition.CrossUnitPrice, calc.getCrossUnitPrice()
+                                                                            .divide(rate, BigDecimal.ROUND_HALF_UP));
+                posIns.add(CISales.QuotationPosition.NetUnitPrice, calc.getNetUnitPrice()
+                                                                            .divide(rate, BigDecimal.ROUND_HALF_UP));
+                posIns.add(CISales.QuotationPosition.CrossPrice, calc.getCrossPrice()
+                                                                            .divide(rate, BigDecimal.ROUND_HALF_UP));
+                posIns.add(CISales.QuotationPosition.NetPrice, calc.getNetPrice()
+                                                                            .divide(rate, BigDecimal.ROUND_HALF_UP));
                 posIns.add(CISales.QuotationPosition.Tax, (calc.getTaxId()).toString());
                 posIns.add(CISales.QuotationPosition.Discount, calc.getDiscountStr());
-                posIns.add(CISales.QuotationPosition.DiscountNetUnitPrice, calc.getDiscountNetUnitPrice());
-                posIns.add(CISales.QuotationPosition.CurrencyId, _parameter.getParameterValue("rateCurrencyId"));
+                posIns.add(CISales.QuotationPosition.DiscountNetUnitPrice, calc.getDiscountNetUnitPrice()
+                                                                            .divide(rate, BigDecimal.ROUND_HALF_UP));
+                posIns.add(CISales.QuotationPosition.CurrencyId, baseCurrInst.getId());
                 posIns.add(CISales.QuotationPosition.Rate, rateObj);
-                posIns.add(CISales.QuotationPosition.RateCurrencyId, _parameter.getParameterValue("rateCurrencyId"));
+                posIns.add(CISales.QuotationPosition.RateCurrencyId, rateCurrInst.getId());
                 posIns.execute();
                 _createdDoc.addPosition(posIns.getInstance());
             }
