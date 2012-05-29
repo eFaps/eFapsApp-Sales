a049e8e217fe59a9d147737d64abf848f798ddb2
m.aranya@moxter.net <m.aranya@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-05-29 00:22:18 +0000

    - adding new method to createDoc and createPositions into CreditNote.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@7555 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/CreditNote_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/CreditNote_Base.java
@@ -21,7 +21,6 @@
 package org.efaps.esjp.sales.document;
 
 import java.math.BigDecimal;
-import java.text.ParseException;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,12 +35,13 @@ import org.efaps.db.Instance;
 import org.efaps.esjp.ci.CIERP;
 import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.sales.Calculator;
-import org.efaps.esjp.sales.Calculator_Base;
+import org.efaps.util.DateTimeUtil;
 import org.efaps.util.EFapsException;
+import org.joda.time.DateMidnight;
 
 /**
  * TODO comment!
- *
+ * 
  * @author The eFaps Team
  * @version $Id$
  */
@@ -53,7 +53,7 @@ public abstract class CreditNote_Base
 
     /**
      * Method for create a new Credit Note.
-     *
+     * 
      * @param _parameter Parameter as passed from eFaps API.
      * @return new Return.
      * @throws EFapsException on error.
@@ -61,78 +61,94 @@ public abstract class CreditNote_Base
     public Return create(final Parameter _parameter)
         throws EFapsException
     {
-        try {
-            final String date = _parameter.getParameterValue("date");
-            final List<Calculator> calcList = analyseTable(_parameter, null);
-            final Instance baseCurrInst = SystemConfiguration.get(
-                            UUID.fromString("c9a1cbc3-fd35-4463-80d2-412422a3802f")).getLink("CurrencyBase");
-            final Instance rateCurrInst = Instance.get(CIERP.Currency.getType(), _parameter
-                            .getParameterValue("rateCurrencyId"));
-            final Object[] rateObj = getRateObject(_parameter);
-            final BigDecimal rate = (BigDecimal) Calculator_Base.getFormatInstance().parse(
-                            _parameter.getParameterValue("rate"));
-            final Long contactid = Instance.get(_parameter.getParameterValue("contact")).getId();
-            final Insert insert = new Insert(CISales.CreditNote);
-            insert.add(CISales.CreditNote.Contact, contactid.toString());
-            insert.add(CISales.CreditNote.CrossTotal, getCrossTotalStr(calcList));
-            insert.add(CISales.CreditNote.RateCrossTotal, getCrossTotalStr(calcList));
-            insert.add(CISales.CreditNote.NetTotal, getNetTotalStr(calcList));
-            insert.add(CISales.CreditNote.RateNetTotal, getNetTotalStr(calcList));
-            insert.add(CISales.CreditNote.DiscountTotal, BigDecimal.ZERO);
-            insert.add(CISales.CreditNote.RateDiscountTotal, BigDecimal.ZERO);
-            insert.add(CISales.CreditNote.CurrencyId, baseCurrInst.getId());
-            insert.add(CISales.CreditNote.RateCurrencyId, rateCurrInst.getId());
-            insert.add(CISales.CreditNote.Rate, rateObj);
-            insert.add(CISales.CreditNote.Date, date);
-            insert.add(CISales.CreditNote.Note, _parameter.getParameterValue("note"));
-            insert.add(CISales.CreditNote.DueDate, _parameter.getParameterValue("dueDate"));
-            insert.add(CISales.CreditNote.Salesperson, _parameter.getParameterValue("salesperson"));
-            insert.add(CISales.CreditNote.Status,
-                                ((Long) Status.find(CISales.CreditNoteStatus.uuid, "Open").getId()).toString());
-            insert.add(CISales.CreditNote.Name, _parameter.getParameterValue("name4create"));
-
-            insert.execute();
-            Integer i = 0;
-            for (final Calculator calc : calcList) {
-                if (!calc.isEmpty()) {
-                    final Insert posIns = new Insert(CISales.CreditNotePosition);
-                    final Long productdId = Instance.get(_parameter.getParameterValues("product")[i]).getId();
-                    posIns.add(CISales.CreditNotePosition.CreditNote, insert.getId());
-                    posIns.add(CISales.CreditNotePosition.PositionNumber, i);
-                    posIns.add(CISales.CreditNotePosition.Product, productdId);
-                    posIns.add(CISales.CreditNotePosition.ProductDesc, _parameter.getParameterValues("productDesc")[i]);
-                    posIns.add(CISales.CreditNotePosition.Quantity, calc.getQuantity());
-                    posIns.add(CISales.CreditNotePosition.UoM, _parameter.getParameterValues("uoM")[i]);
-                    posIns.add(CISales.CreditNotePosition.Tax, calc.getTaxId());
-                    posIns.add(CISales.CreditNotePosition.Discount, calc.getDiscount());
-                    posIns.add(CISales.CreditNotePosition.CurrencyId, rateCurrInst.getId());
-                    posIns.add(CISales.CreditNotePosition.Rate, rateObj);
-                    posIns.add(CISales.CreditNotePosition.RateCurrencyId, rateCurrInst.getId());
-                    if (baseCurrInst.equals(rateCurrInst)) {
-                        posIns.add(CISales.CreditNotePosition.CrossUnitPrice, calc.getCrossUnitPrice());
-                        posIns.add(CISales.CreditNotePosition.NetUnitPrice, calc.getNetUnitPrice());
-                        posIns.add(CISales.CreditNotePosition.CrossPrice, calc.getCrossPrice());
-                        posIns.add(CISales.CreditNotePosition.NetPrice, calc.getNetPrice());
-                        posIns.add(CISales.CreditNotePosition.DiscountNetUnitPrice, calc.getDiscountNetUnitPrice());
-                    } else {
-                        posIns.add(CISales.CreditNotePosition.CrossUnitPrice,
-                                                calc.getCrossUnitPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
-                        posIns.add(CISales.CreditNotePosition.NetUnitPrice,
-                                                calc.getNetUnitPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
-                        posIns.add(CISales.CreditNotePosition.CrossPrice,
-                                                calc.getCrossPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
-                        posIns.add(CISales.CreditNotePosition.NetPrice,
-                                                calc.getNetPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
-                        posIns.add(CISales.CreditNotePosition.DiscountNetUnitPrice,
-                                                calc.getDiscountNetUnitPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
-                    }
-                    posIns.execute();
-                }
-                i++;
-            }
-        } catch (final ParseException e) {
-            throw new EFapsException(Invoice.class, "create.ParseException", e);
-        }
+        createDoc(_parameter);
         return new Return();
+    }
+
+    protected CreatedDoc createDoc(final Parameter _parameter)
+        throws EFapsException
+    {
+        final String date = _parameter.getParameterValue("date");
+        final List<Calculator> calcList = analyseTable(_parameter, null);
+        final Instance baseCurrInst = SystemConfiguration.get(
+                        UUID.fromString("c9a1cbc3-fd35-4463-80d2-412422a3802f")).getLink("CurrencyBase");
+        final Instance rateCurrInst = Instance.get(CIERP.Currency.getType(), _parameter
+                        .getParameterValue("rateCurrencyId"));
+        final Object[] rateObj = getRateObject(_parameter);
+        final BigDecimal rate = ((BigDecimal) rateObj[0]).divide((BigDecimal) rateObj[1], 12,
+                        BigDecimal.ROUND_HALF_UP);
+        final Long contactid = Instance.get(_parameter.getParameterValue("contact")).getId();
+        final Insert insert = new Insert(CISales.CreditNote);
+        insert.add(CISales.CreditNote.Contact, contactid);
+        insert.add(CISales.CreditNote.CrossTotal, getCrossTotal(calcList).divide(rate, BigDecimal.ROUND_HALF_UP));
+        insert.add(CISales.CreditNote.NetTotal, getNetTotal(calcList).divide(rate, BigDecimal.ROUND_HALF_UP));
+        insert.add(CISales.CreditNote.RateNetTotal, getNetTotal(calcList));
+        insert.add(CISales.CreditNote.RateCrossTotal, getCrossTotal(calcList));
+        insert.add(CISales.CreditNote.DiscountTotal, BigDecimal.ZERO);
+        insert.add(CISales.CreditNote.RateDiscountTotal, BigDecimal.ZERO);
+        insert.add(CISales.CreditNote.CurrencyId, baseCurrInst.getId());
+        insert.add(CISales.CreditNote.RateCurrencyId, rateCurrInst.getId());
+        insert.add(CISales.CreditNote.Rate, rateObj);
+        insert.add(CISales.CreditNote.Date, date == null ? DateTimeUtil.normalize(new DateMidnight().toDateTime()) : date);
+        insert.add(CISales.CreditNote.Note, _parameter.getParameterValue("note"));
+        insert.add(CISales.CreditNote.DueDate, _parameter.getParameterValue("dueDate"));
+        insert.add(CISales.CreditNote.Salesperson, _parameter.getParameterValue("salesperson"));
+        insert.add(CISales.CreditNote.Status,
+                        ((Long) Status.find(CISales.CreditNoteStatus.uuid, "Open").getId()).toString());
+        insert.add(CISales.CreditNote.Name, getDocName4Create(_parameter));
+        insert.execute();
+
+        final CreatedDoc createdDoc = new CreatedDoc(insert.getInstance());
+        createPositions(_parameter, calcList, createdDoc);
+        return createdDoc;
+    }
+
+    @Override
+    protected void createPositions(final Parameter _parameter,
+                                   final List<Calculator> _calcList,
+                                   final CreatedDoc _createdDoc)
+        throws EFapsException
+    {
+        // Sales-Configuration
+        final Instance baseCurrInst = SystemConfiguration.get(
+                        UUID.fromString("c9a1cbc3-fd35-4463-80d2-412422a3802f")).getLink("CurrencyBase");
+        final Instance rateCurrInst = Instance.get(CIERP.Currency.getType(),
+                        _parameter.getParameterValue("rateCurrencyId"));
+
+        final Object[] rateObj = getRateObject(_parameter);
+        final BigDecimal rate = ((BigDecimal) rateObj[0]).divide((BigDecimal) rateObj[1], 12,
+                        BigDecimal.ROUND_HALF_UP);
+        Integer i = 0;
+        for (final Calculator calc : _calcList) {
+            if (!calc.isEmpty()) {
+                final Insert posIns = new Insert(CISales.CreditNotePosition);
+                final Long productdId = Instance.get(_parameter.getParameterValues("product")[i]).getId();
+                posIns.add(CISales.CreditNotePosition.CreditNote, _createdDoc.getInstance().getId());
+                posIns.add(CISales.CreditNotePosition.PositionNumber, i);
+                posIns.add(CISales.CreditNotePosition.Product, productdId);
+                posIns.add(CISales.CreditNotePosition.ProductDesc, _parameter.getParameterValues("productDesc")[i]);
+                posIns.add(CISales.CreditNotePosition.Quantity, calc.getQuantity());
+                posIns.add(CISales.CreditNotePosition.UoM, _parameter.getParameterValues("uoM")[i]);
+                posIns.add(CISales.CreditNotePosition.Tax, calc.getTaxId());
+                posIns.add(CISales.CreditNotePosition.Discount, calc.getDiscount());
+                posIns.add(CISales.CreditNotePosition.CurrencyId, baseCurrInst.getId());
+                posIns.add(CISales.CreditNotePosition.Rate, rateObj);
+                posIns.add(CISales.CreditNotePosition.RateCurrencyId, rateCurrInst.getId());
+                posIns.add(CISales.CreditNotePosition.CrossUnitPrice,
+                                calc.getCrossUnitPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
+                posIns.add(CISales.CreditNotePosition.NetUnitPrice,
+                                calc.getNetUnitPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
+                posIns.add(CISales.CreditNotePosition.CrossPrice,
+                                calc.getCrossPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
+                posIns.add(CISales.CreditNotePosition.NetPrice,
+                                calc.getNetPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
+                posIns.add(CISales.CreditNotePosition.DiscountNetUnitPrice,
+                                calc.getDiscountNetUnitPrice().divide(rate, BigDecimal.ROUND_HALF_UP));
+                add2PositionInsert(_parameter, calc, posIns);
+                posIns.execute();
+                _createdDoc.addPosition(posIns.getInstance());
+            }
+            i++;
+        }
     }
 }
