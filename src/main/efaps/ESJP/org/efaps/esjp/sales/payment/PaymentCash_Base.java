f4895f0c6ac604bee4f991c6883173f34c3ce623
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-10-26 22:33:09 +0000

    - sales: adding cash payment document
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@8124 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/payment/PaymentCash_Base.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2003 - 2012 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.sales.payment;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.util.EFapsException;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id: Payment_Base.java 7671 2012-06-14 17:25:53Z
+ *          jorge.cueva@moxter.net $
+ */
+@EFapsUUID("7cb7cef0-52ce-4cf5-858c-b2ea77342ee4")
+@EFapsRevision("$Rev$")
+public abstract class PaymentCash_Base
+    extends AbstractPaymentDocument
+{
+
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @return new Return
+     * @throws EFapsException on error
+     */
+    public Return create(final Parameter _parameter)
+        throws EFapsException
+    {
+        final CreatedDoc createdDoc = createDoc(_parameter);
+        createPayment(_parameter, createdDoc);
+        return new Return();
+    }
+}
