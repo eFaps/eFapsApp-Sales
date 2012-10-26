b997a2c5227e99449ef277d95af32a3b27e1205f
m.aranya@moxter.net <m.aranya@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-10-26 15:23:50 +0000

    - sales: adding esjp to sales payment document childs.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@8100 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/payment/PaymentDeposit_Base.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2003 - 2012 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.sales.payment;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+/**
+ * TODO comment!
+ * 
+ * @author The eFaps Team
+ * @version $Id: Payment_Base.java 7671 2012-06-14 17:25:53Z
+ *          jorge.cueva@moxter.net $
+ */
+@EFapsUUID("c4383bf6-9cca-492b-906f-37fb2e0c9ea2")
+@EFapsRevision("$Rev$")
+public abstract class PaymentDeposit_Base
+    extends Payment
+{
+
+}
