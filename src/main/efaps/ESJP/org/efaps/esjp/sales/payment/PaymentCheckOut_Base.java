0ca6b4fc9caf7ced51690106e6116c59158d6436
m.aranya@moxter.net <m.aranya@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-11-10 00:10:29 +0000

    - sales: adding payment document out to check and deposit, adding esjp to types, update system configuration, and DBProperties.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@8190 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/payment/PaymentCheckOut_Base.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2003 - 2012 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.sales.payment;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.util.EFapsException;
+
+/**
+ * TODO comment!
+ * 
+ * @author The eFaps Team
+ * @version $Id: Payment_Base.java 7671 2012-06-14 17:25:53Z
+ *          jorge.cueva@moxter.net $
+ */
+@EFapsUUID("541288ca-b89a-4b1c-97ad-f01eba31ccaa")
+@EFapsRevision("$Rev$")
+public abstract class PaymentCheckOut_Base
+    extends AbstractPaymentDocument
+{
+
+    /**
+     * @param _parameter Parameter as passed by the eFaps API
+     * @return new Return
+     * @throws EFapsException on error
+     */
+    public Return create(final Parameter _parameter)
+        throws EFapsException
+    {
+        final CreatedDoc createdDoc = createDoc(_parameter);
+        createPayment(_parameter, createdDoc);
+        return new Return();
+    }
+
+}
