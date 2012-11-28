ef3658600e073f71d16384bc597976185922ae5e
m.aranya@moxter.net <m.aranya@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-11-28 01:53:45 +0000

    - sales : adding new menu to show incominginvoice, and create a payments schedule multiples. removing purpose abstract to type payment schedule position.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@8258 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/PaymentSchedule_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/PaymentSchedule_Base.java
@@ -2,14 +2,19 @@ package org.efaps.esjp.sales;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Map;
 import java.util.UUID;
 
 import org.efaps.admin.common.SystemConfiguration;
 import org.efaps.admin.datamodel.Status;
+import org.efaps.admin.dbproperty.DBProperties;
 import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Parameter.ParameterValues;
 import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Context;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
 import org.efaps.db.PrintQuery;
@@ -20,17 +25,19 @@ import org.efaps.util.EFapsException;
 
 /**
  * TODO comment!
- *
+ * 
  * @author The eFaps Team
  * @version $Id: PaymentSchedule_Base.java $
  */
 @EFapsUUID("806c701d-ca2a-4e71-b6b1-7777a77299e4")
 @EFapsRevision("$Rev: 1$")
-public class PaymentSchedule_Base extends EventSchedule
+public class PaymentSchedule_Base
+    extends EventSchedule
 {
 
     /**
      * Method for create a new PaymentSchedule.
+     * 
      * @param _parameter Parameter as passed from eFaps API.
      * @return new Return.
      * @throws EFapsException on error.
@@ -68,12 +75,12 @@ public class PaymentSchedule_Base extends EventSchedule
     }
 
     protected void createPositions(final Parameter _parameter,
-                                 final CreateSchedule createSchedule)
+                                   final CreateSchedule createSchedule)
         throws EFapsException
     {
         final String oids[] = _parameter.getParameterValues("document");
         Integer i = 0;
-        for(final String oid : oids)
+        for (final String oid : oids)
         {
             final Instance instDoc = Instance.get(oid);
             final PrintQuery printDoc = new PrintQuery(instDoc);
@@ -85,11 +92,33 @@ public class PaymentSchedule_Base extends EventSchedule
             insertPayShePos.add(CISales.PaymentSchedulePosition.PaymentSchedule, createSchedule.getInstance().getId());
             insertPayShePos.add(CISales.PaymentSchedulePosition.Document, instDoc.getId());
             insertPayShePos.add(CISales.PaymentSchedulePosition.PositionNumber, i);
-            insertPayShePos.add(CISales.PaymentSchedulePosition.DocumentDesc, _parameter.getParameterValues("documentDesc")[i]);
+            if (_parameter.getParameterValues("documentDesc") != null) {
+                insertPayShePos.add(CISales.PaymentSchedulePosition.DocumentDesc,
+                                _parameter.getParameterValues("documentDesc")[i]);
+            } else {
+                insertPayShePos.add(CISales.PaymentSchedulePosition.DocumentDesc,
+                                DBProperties.getProperty("org.efaps.esjp.sales.PaymentSchedule.defaultDescription"));
+            }
             insertPayShePos.add(CISales.PaymentSchedulePosition.NetPrice, printDoc.getAttribute(CISales.DocumentSumAbstract.CrossTotal));
             insertPayShePos.execute();
             i++;
         }
+    }
+
+    public Return createSchedule4SelectedDocs(final Parameter _parameter)
+        throws EFapsException
+    {
+        final String[] others = (String[]) Context.getThreadContext().getSessionAttribute("internalOIDs");
+        if (others != null) {
+            final String name = (String) getNameFieldValueUI(_parameter).get(ReturnValues.VALUES);
+            @SuppressWarnings("unchecked") final Map<String, String[]> parameters = (Map<String, String[]>) _parameter
+                            .get(ParameterValues.PARAMETERS);
+            parameters.put("name4create", new String[] { name });
+            parameters.put("document", others);
+            _parameter.put(ParameterValues.PARAMETERS, parameters);
+            createSchedule(_parameter);
+        }
+        return new Return();
     }
 
     public Return deleteTrigger(final Parameter _parameter)
