1ae1632424ff963ae130fb6a7a5ba73d5a6cbc0d
rosa.ordonez@efaps.org <rosa.ordonez@efaps.org@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-04-05 21:09:44 +0000

    - Sales: Create a connection between HumanResource_Employee and PaymentOrder document
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@9160 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/PaymentOrder_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/PaymentOrder_Base.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2012 The eFaps Team
+ * Copyright 2003 - 2013 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,8 @@ import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
+import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.QueryBuilder;
 import org.efaps.esjp.ci.CIERP;
 import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.erp.CurrencyInst;
@@ -53,7 +55,24 @@ public abstract class PaymentOrder_Base
     public Return create(final Parameter _parameter)
         throws EFapsException
     {
-        createDoc(_parameter);
+        final CreatedDoc docInst = createDoc(_parameter);
+        final String applicant = _parameter.getParameterValue("applicant");
+        // connect to a applicant if it's given
+        if (applicant != null && applicant.length() > 0) {
+            final Instance employeeInst = Instance.get(applicant);
+            // HumanResource_Employee
+            final QueryBuilder queryBldr = new QueryBuilder(UUID.fromString("682bea7b-03f0-4a31-bd96-cb3a3b94882f"));
+            queryBldr.addWhereAttrEqValue("ID", employeeInst.getId());
+            final MultiPrintQuery multi = queryBldr.getPrint();
+            multi.execute();
+            while (multi.next()) {
+                // HumanResource_Employee2WorkOrder
+                final Insert relInsert = new Insert(UUID.fromString("36ac69db-33d7-4647-afa2-4553ec267119"));
+                relInsert.add("FromLink", employeeInst.getId());
+                relInsert.add("ToLink", docInst.getInstance().getId());
+                relInsert.execute();
+            }
+        }
         return new Return();
     }
 
@@ -71,7 +90,7 @@ public abstract class PaymentOrder_Base
                         : Instance.get(CIERP.Currency.getType(), _parameter.getParameterValue("rateCurrencyId"));
 
         final String rateCrossTotal = _parameter.getParameterValue("rateCrossTotal");
-        BigDecimal ratcross = new BigDecimal(rateCrossTotal);
+        final BigDecimal ratcross = new BigDecimal(rateCrossTotal);
 
         final PriceUtil util = new PriceUtil();
         final BigDecimal[] rates = util.getExchangeRate(util.getDateFromParameter(_parameter), rateCurrInst);
