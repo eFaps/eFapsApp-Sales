d83835b8473d3161a282bc9d924bce78c23c94c3
m.aranya@moxter.net <m.aranya@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-04-09 03:17:33 +0000

    - sales : moved method to class abstract.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@9178 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/payment/DocumentUpdate_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/payment/DocumentUpdate_Base.java
@@ -20,15 +20,33 @@
 
 package org.efaps.esjp.sales.payment;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
 import org.efaps.admin.datamodel.Status;
 import org.efaps.admin.datamodel.Type;
+import org.efaps.admin.event.EventDefinition;
+import org.efaps.admin.event.EventType;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Parameter.ParameterValues;
+import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.admin.ui.Command;
+import org.efaps.db.Checkin;
 import org.efaps.db.Instance;
+import org.efaps.db.PrintQuery;
+import org.efaps.db.SelectBuilder;
 import org.efaps.db.Update;
 import org.efaps.esjp.ci.CIERP;
+import org.efaps.esjp.ci.CISales;
+import org.efaps.esjp.common.jasperreport.StandartReport;
+import org.efaps.esjp.sales.document.AbstractDocument_Base;
 import org.efaps.util.EFapsException;
 
 /**
@@ -144,5 +162,104 @@ public abstract class DocumentUpdate_Base
         throws EFapsException
     {
         return false;
+    }
+
+    public Return renew(final Parameter _parameter)
+        throws EFapsException
+    {
+        Return ret = new Return();
+
+        final Instance instance = _parameter.getInstance();
+
+        if (instance.getType().getUUID().equals(CISales.PaymentCash.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentCash");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentCheck.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentCheck");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentCreditCardAmericanExpress.uuid)) {
+            createFile(_parameter, instance,
+                            "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentCreditCardAmericanExpress");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentCreditCardDiners.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentCreditCardDiners");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentCreditCardMastercard.uuid)) {
+            createFile(_parameter, instance,
+                            "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentCreditCardMastercard");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentCreditCardVisa.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentCreditCardVisa");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentDebitCardVisa.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentDebitCardVisa");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentDeposit.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentDeposit");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentDetraction.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentDetraction");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentExchange.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentExchange");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentRetention.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentMyDesk_Menu_Action_CreatePaymentRetention");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentCashOut.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentOutMyDesk_Menu_Action_CreatePaymentCashOut");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentCheckOut.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentOutMyDesk_Menu_Action_CreatePaymentCheckOut");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentDepositOut.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentOutMyDesk_Menu_Action_CreatePaymentDepositOut");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentDetractionOut.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentOutMyDesk_Menu_Action_CreatePaymentDetractionOut");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentExchangeOut.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentOutMyDesk_Menu_Action_CreatePaymentExchangeOut");
+        } else if (instance.getType().getUUID().equals(CISales.PaymentRetentionOut.uuid)) {
+            createFile(_parameter, instance, "Sales_PaymentDocumentOutMyDesk_Menu_Action_CreatePaymentRetentionOut");
+        }
+        return ret;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected void createFile(final Parameter _parameter,
+                              final Instance _instance,
+                              final String _cmd)
+        throws EFapsException
+    {
+        final Command cmd = Command.get(_cmd);
+        final List<EventDefinition> events = cmd.getEvents(EventType.UI_COMMAND_EXECUTE);
+        final EventDefinition def = events.get(0);
+        final String reportName = def.getProperty("JasperReport");
+        final String mime2 = def.getProperty("Mime");
+        final String clazz = def.getProperty("DataSourceClass");
+
+        final PrintQuery print = new PrintQuery(_instance);
+        final SelectBuilder selFileName = new SelectBuilder().file().label();
+        print.addSelect(selFileName);
+        print.addAttribute("Name");
+        print.execute();
+        String name = print.<String>getSelect(selFileName);
+        _parameter.get(ParameterValues.PROPERTIES);
+        _parameter.put(ParameterValues.INSTANCE, _instance);
+        final Map<String, String> props = (Map<String, String>) _parameter.get(ParameterValues.PROPERTIES);
+        props.put("JasperReport", reportName);
+        String mime = _parameter.getParameterValue("mime");
+        if (!(mime != null && mime.length() > 0)) {
+            mime = mime2 != null ? mime2 : "txt";
+        }
+        props.put("Mime", mime);
+        if (clazz != null) {
+            props.put("DataSourceClass", clazz);
+        }
+        if (name != null && !name.isEmpty()) {
+            name = name.substring(0, name.lastIndexOf("."));
+        } else {
+            name = _instance.getType().getLabel() + "_" + print.getAttribute("Name");
+        }
+        final StandartReport report = new StandartReport();
+        report.setFileName(name);
+        final Return ret = report.execute(_parameter);
+
+        final File file = (File) ret.get(ReturnValues.VALUES);
+        InputStream input = null;
+        try {
+            input = new FileInputStream(file);
+        } catch (final FileNotFoundException e) {
+            throw new EFapsException(AbstractDocument_Base.class, "createFile", e);
+        }
+
+        final Checkin checkin = new Checkin(_instance);
+        checkin.execute(name + "." + mime, input, ((Long) file.length()).intValue());
     }
 }
