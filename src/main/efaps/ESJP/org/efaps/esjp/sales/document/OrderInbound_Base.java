1178ee6fc3302f2d9da89743f88fa442d1957885
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-05-21 15:30:30 +0000

    - sales: adding execution of processes
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@9478 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/OrderInbound_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/OrderInbound_Base.java
@@ -51,6 +51,7 @@ public abstract class OrderInbound_Base
     {
         final CreatedDoc createdDoc = createDoc(_parameter);
         createPositions(_parameter, createdDoc);
+        executeProcess(_parameter, createdDoc);
         return new Return();
     }
 }
