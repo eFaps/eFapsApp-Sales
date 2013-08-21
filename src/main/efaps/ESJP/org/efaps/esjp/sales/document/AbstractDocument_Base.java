fd7181ce41a33092184a20c310d53c33c571b223
jorge.cueva@moxter.net <jorge.cueva@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-08-21 20:01:55 +0000

    - sales: update method to set the currency in a document form.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@10081 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/AbstractDocument_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/AbstractDocument_Base.java
@@ -951,54 +951,29 @@ public abstract class AbstractDocument_Base
         final StringBuilder currency = new StringBuilder();
         BigDecimal rate = null;
         BigDecimal[] ratesCur = null;
-        if (_instance.getType().isKindOf(CISales.DocumentSumAbstract.getType())) {
-            if (rates != null) {
-                final Instance currency4Invoice = Sales.getSysConfig().getLink(SalesSettings.CURRENCY4INVOICE);
-                final Instance baseCurrency = Sales.getSysConfig().getLink(SalesSettings.CURRENCYBASE);
-                final Instance instanceDerived = getInstance4Derived(_parameter);
-                boolean derived = false;
-                if (instanceDerived.isValid()) {
-                    derived = true;
-                }
-                final Instance newInst = Instance.get(CIERP.Currency.getType(), rates[2].toString());
-                Context.getThreadContext().setSessionAttribute(AbstractDocument_Base.CURRENCYINST_KEY, newInst);
-                ratesCur = new PriceUtil().getExchangeRate(new DateTime().toDateMidnight().toDateTime(), newInst);
+        if (rates != null) {
+            final Instance currency4Invoice = Sales.getSysConfig().getLink(SalesSettings.CURRENCY4INVOICE);
+            final Instance baseCurrency = Sales.getSysConfig().getLink(SalesSettings.CURRENCYBASE);
+            final Instance instanceDerived = getInstance4Derived(_parameter);
+            boolean derived = false;
+            if (instanceDerived.isValid()) {
+                derived = true;
+            }
+            final Instance newInst = Instance.get(CIERP.Currency.getType(), rates[2].toString());
+            Context.getThreadContext().setSessionAttribute(AbstractDocument_Base.CURRENCYINST_KEY, newInst);
+            ratesCur = new PriceUtil().getExchangeRate(new DateTime().toDateMidnight().toDateTime(), newInst);
 
-                if (rates[2].equals(rates[3]) && !currency4Invoice.equals(baseCurrency) && !derived) {
-                    currency.append(getSetFieldValue(0, "rateCurrencyId", "" + (((Long) rates[2]) - 1)))
-                            .append("\n");
-                    /*currency.append(" require([\"dojo/query\",\"dojo/NodeList-traverse\"], function(query){\n")
-                            .append("query(\"select[name=rateCurrencyId]\")[0].selectedIndex=")
-                            .append(((Long) rates[2]) - 1).append(";");*/
-                    currency.append(getSetFieldValue(0, "rateCurrencyData", ratesCur[1].toString()))
-                            .append(getSetFieldValue(0, "rate", ratesCur[1].toString()))
-                            .append("\n")
-                            .append(getSetFieldValue(0, "rate" + RateUI.INVERTEDSUFFIX,
-                                            "" + new CurrencyInst(newInst).isInvert()))
-                            .append("\n");
-                    /*currency.append(getSetFieldValue(0, "rateCurrencyData", ratesCur[1].toString()))
-                            .append("query(\"input[name=rate]\")[0].value='").append(ratesCur[1].toString()).append("';")
-                            .append("query(\"input[name=rate").append(RateUI.INVERTEDSUFFIX).append("]\")[0].value='")
-                                .append(new CurrencyInst(newInst).isInvert()).append("';")
-                            .append(" });\n");*/
-                } else if (!rates[2].equals(rates[3])) {
+            if ((rates[2].equals(rates[3]) && !currency4Invoice.equals(baseCurrency) && !derived) || !rates[2].equals(rates[3])) {
+                currency.append(getSetFieldValue(0, "rateCurrencyId", "" + ((Long) rates[2])))
+                        .append("\n");
+                currency.append(getSetFieldValue(0, "rateCurrencyData", ratesCur[1].toString()))
+                        .append(getSetFieldValue(0, "rate", ratesCur[1].toString()))
+                        .append("\n")
+                        .append(getSetFieldValue(0, "rate" + RateUI.INVERTEDSUFFIX,
+                                        "" + new CurrencyInst(newInst).isInvert()))
+                        .append("\n");
+                if (!rates[2].equals(rates[3])) {
                     rate = (BigDecimal) rates[1];
-                    currency.append(getSetFieldValue(0, "rateCurrencyId", "" + (((Long) rates[2]) - 1)))
-                            .append("\n");
-                    /*currency.append(" require([\"dojo/query\",\"dojo/NodeList-traverse\"], function(query){\n")
-                            .append("query(\"select[name=rateCurrencyId]\")[0].selectedIndex=")
-                            .append(((Long) rates[2]) - 1).append(";");*/
-                    currency.append(getSetFieldValue(0, "rateCurrencyData", ratesCur[1].toString()))
-                            .append(getSetFieldValue(0, "rate", ratesCur[1].toString()))
-                            .append("\n")
-                            .append(getSetFieldValue(0, "rate" + RateUI.INVERTEDSUFFIX,
-                                            "" + new CurrencyInst(newInst).isInvert()))
-                            .append("\n");
-                    /*currency.append(getSetFieldValue(0, "rateCurrencyData", ratesCur[1].toString()))
-                            .append("query(\"input[name=rate]\")[0].value='").append(ratesCur[1].toString()).append("';")
-                            .append("query(\"input[name=rate").append(RateUI.INVERTEDSUFFIX).append("]\")[0].value='")
-                                .append(new CurrencyInst(newInst).isInvert()).append("';")
-                            .append(" });\n");*/
                 }
             }
         }
@@ -1064,24 +1039,22 @@ public abstract class AbstractDocument_Base
             map.put("productDesc",  multi.<String>getAttribute(CISales.PositionSumAbstract.ProductDesc));
             map.put("uoM", getUoMFieldStr(multi.<Long>getAttribute(CISales.PositionSumAbstract.UoM),
                                             multi.<Long>getSelect(selProdDim)));
-            if (_instance.getType().isKindOf(CISales.DocumentSumAbstract.getType())) {
-                if (TargetMode.EDIT.equals(Context.getThreadContext()
-                                .getSessionAttribute(AbstractDocument_Base.TARGETMODE_DOC_KEY))) {
-                    map.put("netUnitPrice", formaterSysConf.format(rate != null ? rateNetUnitPrice : netUnitPrice));
-                    map.put("discountNetUnitPrice",
-                                    formaterSysConf.format(rate != null ? rateDiscountNetUnitPrice : discountNetUnitPrice));
-                    map.put("netPrice", formater.format(rate != null ? rateNetPrice : netPrice));
-                } else {
-                    map.put("netUnitPrice", formaterSysConf.format(rate != null ? netUnitPrice.divide(rate,
-                                    BigDecimal.ROUND_HALF_UP) : netUnitPrice));
-                    map.put("discountNetUnitPrice", formaterSysConf.format(rate != null ? discountNetUnitPrice.divide(rate,
-                                    BigDecimal.ROUND_HALF_UP)
-                                    : discountNetUnitPrice));
-                    map.put("netPrice", formater.format(rate != null ? netPrice.divide(rate, BigDecimal.ROUND_HALF_UP)
-                                    : netPrice));
-                    map.put("discount",
-                                    formater.format(multi.<BigDecimal>getAttribute(CISales.PositionSumAbstract.Discount)));
-                }
+            if (TargetMode.EDIT.equals(Context.getThreadContext()
+                            .getSessionAttribute(AbstractDocument_Base.TARGETMODE_DOC_KEY))) {
+                map.put("netUnitPrice", formaterSysConf.format(rate != null ? rateNetUnitPrice : netUnitPrice));
+                map.put("discountNetUnitPrice",
+                                formaterSysConf.format(rate != null ? rateDiscountNetUnitPrice : discountNetUnitPrice));
+                map.put("netPrice", formater.format(rate != null ? rateNetPrice : netPrice));
+            } else {
+                map.put("netUnitPrice", formaterSysConf.format(rate != null ? netUnitPrice.divide(rate,
+                                BigDecimal.ROUND_HALF_UP) : netUnitPrice));
+                map.put("discountNetUnitPrice", formaterSysConf.format(rate != null ? discountNetUnitPrice.divide(rate,
+                                BigDecimal.ROUND_HALF_UP)
+                                : discountNetUnitPrice));
+                map.put("netPrice", formater.format(rate != null ? netPrice.divide(rate, BigDecimal.ROUND_HALF_UP)
+                                : netPrice));
+                map.put("discount",
+                                formater.format(multi.<BigDecimal>getAttribute(CISales.PositionSumAbstract.Discount)));
             }
             values.put(multi.<Integer>getAttribute(CISales.PositionSumAbstract.PositionNumber), map);
         }
