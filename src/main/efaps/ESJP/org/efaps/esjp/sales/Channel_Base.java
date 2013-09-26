c11932ee29dbef8f6e0940e905e1985b4daecaab
jorge.cueva@moxter.net <jorge.cueva@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-09-26 16:54:01 +0000

    - sales: add classes for channel type.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@10318 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/Channel_Base.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev: 8120 $
+ * Last Changed:    $Date: 2012-10-26 13:21:34 -0500 (vie, 26 oct 2012) $
+ * Last Changed By: $Author: jorge.cueva@moxter.net $
+ */
+
+package org.efaps.esjp.sales;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id: Channel_Base.java 8120 2012-10-26 18:21:34Z jorge.cueva@moxter.net $
+ */
+@EFapsUUID("4541b746-f653-46d8-b066-9aea7e111766")
+@EFapsRevision("$Rev: 8120 $")
+public class Channel_Base
+{
+
+}
