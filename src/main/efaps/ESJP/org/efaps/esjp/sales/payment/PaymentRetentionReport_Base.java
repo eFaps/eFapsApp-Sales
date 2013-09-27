5b5c7e7e80a3fd65b2caed582c2d51c6d73d4231
jorge.cueva@moxter.net <jorge.cueva@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-09-27 23:56:46 +0000

    - sales: update retention report.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@10326 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/payment/PaymentRetentionReport_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/payment/PaymentRetentionReport_Base.java
@@ -20,7 +20,11 @@
 
 package org.efaps.esjp.sales.payment;
 
+import java.io.File;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -46,7 +50,9 @@ import org.efaps.db.QueryBuilder;
 import org.efaps.db.SelectBuilder;
 import org.efaps.esjp.ci.CIContacts;
 import org.efaps.esjp.ci.CISales;
+import org.efaps.esjp.common.file.FileUtil;
 import org.efaps.esjp.common.jasperreport.EFapsMapDataSource;
+import org.efaps.esjp.common.jasperreport.EFapsTextReport;
 import org.efaps.esjp.common.jasperreport.StandartReport;
 import org.efaps.esjp.erp.util.ERP;
 import org.efaps.esjp.erp.util.ERPSettings;
@@ -65,6 +71,8 @@ import org.joda.time.format.DateTimeFormat;
 public abstract class PaymentRetentionReport_Base
     extends EFapsMapDataSource
 {
+    public static final String format = "0626";
+
     /**
      * Enum used to define the keys for the map.
      */
@@ -78,6 +86,24 @@ public abstract class PaymentRetentionReport_Base
         DOCNUM("documentNumber"),
         /** */
         NAME("name"),
+        /** */
+        LASTNAME("lastName"),
+        /** */
+        LASTNAME2("lastName2"),
+        /** */
+        RETVOUCHERSERIE("retentionVoucherSerie"),
+        /** */
+        RETVOUCHERNUMB("retentionVoucherNumb"),
+        /** */
+        COMPANYNAME("companyName"),
+        /** */
+        VOUCHERTYPE("voucherType"),
+        /** */
+        VOUCHERSERIE("voucherSerie"),
+        /** */
+        VOUCHERNUMB("voucherNumb"),
+        /** */
+        VOUCHERDATE("voucherDate"),
         /** */
         CROSSAMOUNT("crossAmount"),
         /** */
@@ -237,6 +263,7 @@ public abstract class PaymentRetentionReport_Base
     public Return createDocReport(final Parameter _parameter)
         throws EFapsException
     {
+        Return ret = new Return();
         final String dateFrom = _parameter.getParameterValue("dateFrom");
         final String dateTo = _parameter.getParameterValue("dateTo");
         final String mime = _parameter.getParameterValue("mime");
@@ -244,11 +271,18 @@ public abstract class PaymentRetentionReport_Base
         props.put("Mime", mime);
         final DateTime from = new DateTime(dateFrom);
         final DateTime to = new DateTime(dateTo);
-        final StandartReport report = new StandartReport();
-        buildHeader(_parameter, report);
-        report.setFileName(getReportName(_parameter, from, to));
-        report.getJrParameters().put("Mime", mime);
-        return report.execute(_parameter);
+        if ("txt".equalsIgnoreCase(mime)) {
+            ret = createTextReport(_parameter);
+        } else {
+            final StandartReport report = new StandartReport();
+            buildHeader(_parameter, report);
+            report.setFileName(getReportName(_parameter, from, to));
+            report.getJrParameters().put("Mime", mime);
+
+            ret = report.execute(_parameter);
+        }
+
+        return ret;
     }
 
     protected void buildHeader(final Parameter _parameter,
@@ -278,5 +312,223 @@ public abstract class PaymentRetentionReport_Base
         return DBProperties.getProperty("Sales_PaymentRetentionReport.Label", "es")
                         + "-" + _from.toString(DateTimeFormat.shortDate())
                         + "-" + _to.toString(DateTimeFormat.shortDate());
+    }
+
+    public Return createTextReport(final Parameter _parameter)
+        throws EFapsException
+    {
+        return new EFapsTextReport()
+        {
+            @Override
+            protected File getEmptyFile4TextReport()
+                throws EFapsException
+            {
+                final SystemConfiguration erpConfig = ERP.getSysConfig();
+                final String companyNumber = erpConfig.getAttributeValue(ERPSettings.COMPANYTAX);
+                final String dateFrom = _parameter.getParameterValue("dateFrom");
+                final String dateTo = _parameter.getParameterValue("dateTo");
+                final DateTime from = new DateTime(dateFrom);
+                final DateTime to = new DateTime(dateTo);
+                final String name = buildName4TextReport(format, "" + from.getYear(),
+                                "" + from.getMonthOfYear(), companyNumber);
+                final File file = new FileUtil().getFile(name, "igv");
+                return file;
+            }
+
+            @Override
+            protected void addHeaderDefinition(final Parameter _parameter,
+                                               final List<ColumnDefinition> _columnsList)
+                throws EFapsException
+            {
+            }
+
+            @Override
+            protected List<Object> getHeaderData(final Parameter _parameter)
+                throws EFapsException
+            {
+                return null;
+            }
+
+            @Override
+            protected void addColumnDefinition(final Parameter _parameter,
+                                               final List<ColumnDefinition> _columnsList)
+                throws EFapsException
+            {
+                final DecimalFormat decimalFormat = (DecimalFormat) NumberFormat.getInstance(Context.getThreadContext()
+                                .getLocale());
+                decimalFormat.setMaximumIntegerDigits(12);
+                decimalFormat.setMaximumFractionDigits(2);
+                decimalFormat.setMinimumFractionDigits(2);
+                decimalFormat.setGroupingUsed(false);
+                decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
+
+                _columnsList.add(new ColumnDefinition("|", 11, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 40, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 20, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 20, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 20, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 4, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 8, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 10, 0, null, new SimpleDateFormat("dd/MM/yyyy"),
+                                "", Type.DATETYPE, "%1$-10s"));
+                _columnsList.add(new ColumnDefinition("|", 12, 2, true, decimalFormat, "0", Type.NUMBERTYPE, null));
+
+                _columnsList.add(new ColumnDefinition("|", 2, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 4, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 15, 0, null, null, "", Type.STRINGTYPE, null));
+                _columnsList.add(new ColumnDefinition("|", 10, 0, null, new SimpleDateFormat("dd/MM/yyyy"),
+                                "", Type.DATETYPE, "%1$-10s"));
+                _columnsList.add(new ColumnDefinition("|", 12, 2, true, decimalFormat, "0", Type.NUMBERTYPE, null));
+            }
+
+            @Override
+            protected List<List<Object>> createDataSource(final Parameter _parameter)
+                throws EFapsException
+            {
+                final List<List<Object>> lst = new ArrayList<List<Object>>();
+
+
+                final DateTime dateFrom = new DateTime(_parameter.getParameterValue("dateFrom"));
+                final DateTime dateTo = new DateTime(_parameter.getParameterValue("dateTo"));
+
+                final QueryBuilder attrQueryBldr = new QueryBuilder(CISales.PaymentRetentionOut);
+                attrQueryBldr.addWhereAttrGreaterValue(CISales.PaymentRetentionOut.Date, dateFrom.minusMinutes(1));
+                attrQueryBldr.addWhereAttrLessValue(CISales.PaymentRetentionOut.Date, dateTo.plusMinutes(1));
+                final AttributeQuery attrQuery = attrQueryBldr.getAttributeQuery(CISales.PaymentRetentionOut.ID);
+
+                final QueryBuilder queryBldr = new QueryBuilder(CISales.Payment);
+                queryBldr.addWhereAttrInQuery(CISales.Payment.TargetDocument, attrQuery);
+                final MultiPrintQuery multi = queryBldr.getPrint();
+                multi.addAttribute(CISales.Payment.Amount,
+                                    CISales.Payment.CurrencyLink);
+                final SelectBuilder selContact = new SelectBuilder().linkto(CISales.Payment.CreateDocument)
+                                .linkto(CISales.DocumentSumAbstract.Contact).attribute(CIContacts.Contact.Name);
+                final SelectBuilder selDocNumRuc = new SelectBuilder().linkto(CISales.Payment.CreateDocument)
+                                .linkto(CISales.DocumentSumAbstract.Contact).clazz(CIContacts.ClassOrganisation)
+                                .attribute(CIContacts.ClassOrganisation.TaxNumber);
+                final SelectBuilder selDocNumDni = new SelectBuilder().linkto(CISales.Payment.CreateDocument)
+                                .linkto(CISales.DocumentSumAbstract.Contact).clazz(CIContacts.ClassPerson)
+                                .attribute(CIContacts.ClassPerson.IdentityCard);
+                final SelectBuilder selCrossTotal = new SelectBuilder().linkto(CISales.Payment.CreateDocument)
+                                .attribute(CISales.DocumentSumAbstract.RateCrossTotal);
+                final SelectBuilder selCreateDocName = new SelectBuilder().linkto(CISales.Payment.CreateDocument)
+                                .attribute(CISales.DocumentSumAbstract.Name);
+                final SelectBuilder selCreateDocDate = new SelectBuilder().linkto(CISales.Payment.CreateDocument)
+                                .attribute(CISales.DocumentSumAbstract.Date);
+                final SelectBuilder selTargetDocDate = new SelectBuilder().linkto(CISales.Payment.TargetDocument)
+                                .attribute(CISales.PaymentRetentionOut.Date);
+                final SelectBuilder selTargetDocName = new SelectBuilder().linkto(CISales.Payment.TargetDocument)
+                                .attribute(CISales.PaymentRetentionOut.Name);
+                multi.addSelect(selContact, selDocNumDni, selDocNumRuc, selCreateDocDate,
+                                selCrossTotal, selTargetDocDate, selCreateDocName, selTargetDocName);
+                multi.execute();
+                final List<Map<String, Object>> lstMap = new ArrayList<Map<String, Object>>();
+                while (multi.next()) {
+                    final Map<String, Object> map = new HashMap<String, Object>();
+                    final String contactName = multi.<String>getSelect(selContact);
+                    String docNum = multi.<String>getSelect(selDocNumRuc);
+                    String docType = PaymentRetentionReport_Base.DocType.RUC.getKey();
+                    if (docNum == null) {
+                        docNum = multi.<String>getSelect(selDocNumDni);
+                        if (docNum.length() == 8) {
+                            docType = PaymentRetentionReport_Base.DocType.DNI.getKey();
+                        } else if (docNum.length() == 9) {
+                            docType = PaymentRetentionReport_Base.DocType.CE.getKey();
+                        } else {
+                            docType = PaymentRetentionReport_Base.DocType.OTHER.getKey();
+                        }
+                    }
+                    final DateTime voucherDate = multi.<DateTime>getSelect(selCreateDocDate);
+                    final DateTime retVoucherDate = multi.<DateTime>getSelect(selTargetDocDate);
+                    final BigDecimal crossTot = multi.<BigDecimal>getSelect(selCrossTotal);
+                    final BigDecimal retention = multi.<BigDecimal>getAttribute(CISales.Payment.Amount);
+
+                    map.put(PaymentRetentionReport_Base.Field.DOCTYPE.getKey(), docType);
+                    map.put(PaymentRetentionReport_Base.Field.DOCNUM.getKey(), docNum);
+                    if (docType.equals(PaymentRetentionReport_Base.DocType.RUC.getKey())) {
+                        map.put(PaymentRetentionReport_Base.Field.COMPANYNAME.getKey(), contactName);
+                    } else {
+                        map.put(PaymentRetentionReport_Base.Field.NAME.getKey(), contactName);
+                        map.put(PaymentRetentionReport_Base.Field.LASTNAME.getKey(), null);
+                        map.put(PaymentRetentionReport_Base.Field.LASTNAME2.getKey(), null);
+                    }
+
+                    final String retentionVoucherName = multi.<String>getSelect(selTargetDocName);
+                    final String[] retentionVoucherNameArr = retentionVoucherName.split("-");
+                    if (retentionVoucherNameArr.length == 2) {
+                        final String retVoucherSerie = retentionVoucherNameArr[0];
+                        final String retVoucherNumb = retentionVoucherNameArr[1];
+                        map.put(PaymentRetentionReport_Base.Field.RETVOUCHERSERIE.getKey(), retVoucherSerie);
+                        map.put(PaymentRetentionReport_Base.Field.RETVOUCHERNUMB.getKey(), retVoucherNumb);
+                    } else {
+                        final String retVoucherNumb = retentionVoucherNameArr[0];
+                        map.put(PaymentRetentionReport_Base.Field.RETVOUCHERSERIE.getKey(), null);
+                        map.put(PaymentRetentionReport_Base.Field.RETVOUCHERNUMB.getKey(), retVoucherNumb);
+                    }
+                    map.put(PaymentRetentionReport_Base.Field.RETENTION.getKey(), retention);
+                    map.put(PaymentRetentionReport_Base.Field.PAYDATE.getKey(), retVoucherDate);
+
+                    map.put(PaymentRetentionReport_Base.Field.VOUCHERTYPE.getKey(), "1");
+
+                    final String voucherName = multi.<String>getSelect(selCreateDocName);
+                    final String[] voucherNameArr = voucherName.split("-");
+                    if (voucherNameArr.length == 2) {
+                        final String voucherSerie = voucherNameArr[0];
+                        final String voucherNumb = voucherNameArr[1];
+                        map.put(PaymentRetentionReport_Base.Field.VOUCHERSERIE.getKey(), voucherSerie);
+                        map.put(PaymentRetentionReport_Base.Field.VOUCHERNUMB.getKey(), voucherNumb);
+                    } else {
+                        final String voucherNumb = voucherNameArr[0];
+                        map.put(PaymentRetentionReport_Base.Field.VOUCHERSERIE.getKey(), "");
+                        map.put(PaymentRetentionReport_Base.Field.VOUCHERNUMB.getKey(), voucherNumb);
+                    }
+                    map.put(PaymentRetentionReport_Base.Field.VOUCHERDATE.getKey(), voucherDate);
+                    map.put(PaymentRetentionReport_Base.Field.CROSSAMOUNT.getKey(), crossTot);
+
+                    lstMap.add(map);
+                }
+
+                Collections.sort(lstMap, new Comparator<Map<String, Object>>() {
+
+                    @Override
+                    public int compare(final Map<String, Object> _o1,
+                                       final Map<String, Object> _o2)
+                    {
+                        final DateTime date1 = (DateTime) _o1.get(PaymentRetentionReport_Base.Field.PAYDATE.getKey());
+                        final DateTime date2 = (DateTime) _o2.get(PaymentRetentionReport_Base.Field.PAYDATE.getKey());
+                        final int ret;
+                        if (date1.isEqual(date2)) {
+                            final String sort1 = (String) _o1.get(PaymentRetentionReport_Base.Field.NAME.getKey());
+                            final String sort2 = (String) _o2.get(PaymentRetentionReport_Base.Field.NAME.getKey());
+                            ret = sort1.compareTo(sort2);
+                        } else {
+                            ret = date1.compareTo(date2);
+                        }
+                        return ret;
+                    }
+                });
+
+                for (final Map<String, Object> map : lstMap) {
+                    final List<Object> rowLst = new ArrayList<Object>();
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.DOCNUM.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.COMPANYNAME.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.LASTNAME.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.LASTNAME2.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.NAME.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.RETVOUCHERSERIE.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.RETVOUCHERNUMB.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.PAYDATE.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.RETENTION.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.VOUCHERTYPE.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.VOUCHERSERIE.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.VOUCHERNUMB.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.VOUCHERDATE.getKey()));
+                    rowLst.add(map.get(PaymentRetentionReport_Base.Field.CROSSAMOUNT.getKey()));
+                    lst.add(rowLst);
+                }
+
+                return lst;
+            }
+        }.getTextReport(_parameter);
     }
 }
