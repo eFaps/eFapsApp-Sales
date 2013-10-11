c1b240b9277f2e66ccc265bdebb1367460281311
m.aranya@moxter.net <m.aranya@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-10-11 15:52:46 +0000

    - sales : correction receipt to create and createDoc.
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@10413 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/Receipt_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/Receipt_Base.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.UUID;
 
 import org.efaps.admin.common.SystemConfiguration;
-import org.efaps.admin.datamodel.Status;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.program.esjp.EFapsRevision;
@@ -35,13 +34,11 @@ import org.efaps.db.Instance;
 import org.efaps.esjp.ci.CIERP;
 import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.sales.Calculator;
-import org.efaps.util.DateTimeUtil;
 import org.efaps.util.EFapsException;
-import org.joda.time.DateMidnight;
 
 /**
  * TODO comment!
- * 
+ *
  * @author The eFaps Team
  * @version $Id: Receipt_Base.java 6430 2011-04-19 18:44:28Z
  *          jorge.cueva@moxter.net $
@@ -54,7 +51,7 @@ public abstract class Receipt_Base
 
     /**
      * Method for create a new Receipt.
-     * 
+     *
      * @param _parameter Parameter as passed from eFaps API.
      * @return new Return.
      * @throws EFapsException on error.
@@ -62,126 +59,9 @@ public abstract class Receipt_Base
     public Return create(final Parameter _parameter)
         throws EFapsException
     {
-        createDoc(_parameter);
+        final CreatedDoc doc = createDoc(_parameter);
+        createPositions(_parameter, doc);
         return new Return();
-    }
-
-    /**
-     * Internal Method to create the document and the position.
-     * 
-     * @param _parameter Parameter as passed from eFaps API.
-     * @return new Instance of CreatedDoc.
-     * @throws EFapsException on error.
-     */
-    protected CreatedDoc createDoc(final Parameter _parameter)
-        throws EFapsException
-    {
-        final String date = _parameter.getParameterValue("date");
-        final List<Calculator> calcList = analyseTable(_parameter, null);
-        final Long contactid = Instance.get(_parameter.getParameterValue("contact")).getId();
-        final String name = _parameter.getParameterValue("name4create");
-        // Sales-Configuration
-        final Instance baseCurrInst = SystemConfiguration.get(
-                        UUID.fromString("c9a1cbc3-fd35-4463-80d2-412422a3802f")).getLink("CurrencyBase");
-        final Instance rateCurrInst = Instance.get(CIERP.Currency.getType(),
-                        _parameter.getParameterValue("rateCurrencyId"));
-
-        final Object[] rateObj = getRateObject(_parameter);
-        final BigDecimal rate = ((BigDecimal) rateObj[0]).divide((BigDecimal) rateObj[1], 12,
-                        BigDecimal.ROUND_HALF_UP);
-
-        final Insert insert = new Insert(CISales.Receipt);
-        insert.add(CISales.Receipt.RateCrossTotal, getCrossTotal(calcList).setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-        insert.add(CISales.Receipt.RateNetTotal, getNetTotal(calcList).setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-        insert.add(CISales.Receipt.RateDiscountTotal, BigDecimal.ZERO);
-        insert.add(CISales.Receipt.CrossTotal,
-                        getCrossTotal(calcList).divide(rate, BigDecimal.ROUND_HALF_UP).setScale(isLongDecimal(_parameter),
-                                        BigDecimal.ROUND_HALF_UP));
-        insert.add(CISales.Receipt.NetTotal,
-                        getNetTotal(calcList).divide(rate, BigDecimal.ROUND_HALF_UP).setScale(isLongDecimal(_parameter),
-                                        BigDecimal.ROUND_HALF_UP));
-        insert.add(CISales.Receipt.DiscountTotal, BigDecimal.ZERO);
-        insert.add(CISales.Receipt.Contact, contactid);
-        insert.add(CISales.Receipt.Date, date == null ? DateTimeUtil.normalize(new DateMidnight().toDateTime()) : date);
-        insert.add(CISales.Receipt.Salesperson, _parameter.getParameterValue("salesperson"));
-        insert.add(CISales.Receipt.Name, name);
-        insert.add(CISales.Receipt.Status, ((Long) Status.find("Sales_ReceiptStatus", "Open").getId()).toString());
-        insert.add(CISales.Receipt.Note, _parameter.getParameterValue("note"));
-        insert.add(CISales.Receipt.DueDate, _parameter.getParameterValue("dueDate"));
-        insert.add(CISales.Receipt.CurrencyId, baseCurrInst.getId());
-        insert.add(CISales.Receipt.RateCurrencyId, rateCurrInst.getId());
-        insert.add(CISales.Receipt.Rate, rateObj);
-        insert.execute();
-
-        final CreatedDoc createdDoc = new CreatedDoc(insert.getInstance());
-        createPositions(_parameter, calcList, createdDoc);
-        return createdDoc;
-    }
-
-    /**
-     * Internal Method to create the positions for this Document.
-     * 
-     * @param _parameter Parameter as passed from eFaps API.
-     * @param _calcList List of Calculators
-     * @param _createdDoc cretaed Document
-     * @throws EFapsException on error
-     */
-    protected void createPositions(final Parameter _parameter,
-                                   final List<Calculator> _calcList,
-                                   final CreatedDoc _createdDoc)
-        throws EFapsException
-    {
-        // Sales-Configuration
-        final Instance baseCurrInst = SystemConfiguration.get(
-                        UUID.fromString("c9a1cbc3-fd35-4463-80d2-412422a3802f")).getLink("CurrencyBase");
-        final Instance rateCurrInst = Instance.get(CIERP.Currency.getType(),
-                        _parameter.getParameterValue("rateCurrencyId"));
-
-        final Object[] rateObj = getRateObject(_parameter);
-        final BigDecimal rate = ((BigDecimal) rateObj[0]).divide((BigDecimal) rateObj[1], 12,
-                        BigDecimal.ROUND_HALF_UP);
-        Integer i = 0;
-        for (final Calculator calc : _calcList) {
-            final Insert posIns = new Insert(CISales.ReceiptPosition);
-            final Long productdId = Instance.get(_parameter.getParameterValues("product")[i]).getId();
-            posIns.add(CISales.ReceiptPosition.Receipt, _createdDoc.getInstance().getId());
-            posIns.add(CISales.ReceiptPosition.PositionNumber, i);
-            posIns.add(CISales.ReceiptPosition.Product, productdId);
-            posIns.add(CISales.ReceiptPosition.ProductDesc, _parameter.getParameterValues("productDesc")[i]);
-            posIns.add(CISales.ReceiptPosition.Quantity, calc.getQuantity());
-            posIns.add(CISales.ReceiptPosition.UoM, _parameter.getParameterValues("uoM")[i]);
-            posIns.add(CISales.ReceiptPosition.Tax, calc.getTaxId());
-            posIns.add(CISales.ReceiptPosition.Discount, calc.getDiscount());
-            posIns.add(CISales.ReceiptPosition.CurrencyId, baseCurrInst.getId());
-            posIns.add(CISales.ReceiptPosition.Rate, rateObj);
-            posIns.add(CISales.ReceiptPosition.RateCurrencyId, rateCurrInst.getId());
-            posIns.add(CISales.ReceiptPosition.CrossUnitPrice,
-                            calc.getCrossUnitPrice().divide(rate, BigDecimal.ROUND_HALF_UP)
-                                            .setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.NetUnitPrice,
-                            calc.getNetUnitPrice().divide(rate, BigDecimal.ROUND_HALF_UP)
-                                            .setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.CrossPrice,
-                            calc.getCrossPrice().divide(rate, BigDecimal.ROUND_HALF_UP)
-                                            .setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.NetPrice,
-                            calc.getNetPrice().divide(rate, BigDecimal.ROUND_HALF_UP)
-                                            .setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.DiscountNetUnitPrice, calc.getDiscountNetUnitPrice().divide(rate,
-                            BigDecimal.ROUND_HALF_UP).setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.RateNetUnitPrice,
-                            calc.getNetUnitPrice().setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.RateCrossUnitPrice,
-                            calc.getCrossUnitPrice().setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.RateDiscountNetUnitPrice,
-                            calc.getDiscountNetUnitPrice().setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.RateNetPrice,
-                            calc.getNetPrice().setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.add(CISales.ReceiptPosition.RateCrossPrice,
-                            calc.getCrossPrice().setScale(isLongDecimal(_parameter), BigDecimal.ROUND_HALF_UP));
-            posIns.execute();
-            i++;
-        }
     }
 
     /**
