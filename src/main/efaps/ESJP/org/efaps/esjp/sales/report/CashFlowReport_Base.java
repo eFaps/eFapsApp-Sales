db52545985fbc80bbd234acc102931fa4106d353
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-12-04 19:17:34 +0000

    - sales: update of cashflow
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@11306 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/report/CashFlowReport_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/report/CashFlowReport_Base.java
@@ -22,7 +22,11 @@ package org.efaps.esjp.sales.report;
 
 import java.io.File;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
@@ -63,7 +67,8 @@ import org.slf4j.LoggerFactory;
  * TODO comment!
  *
  * @author The eFaps Team
- * @version $Id$
+ * @version $Id: CashFlowReport_Base.java 11305 2013-12-04 16:16:47Z
+ *          jan@moxter.net $
  */
 @EFapsUUID("67b5c710-4e36-4eb2-95c0-8f04b15500ec")
 @EFapsRevision("$Rev$")
@@ -132,12 +137,15 @@ public abstract class CashFlowReport_Base
         extends AbstractDynamicReport
     {
 
+        private final Map<String,Group> groups = new HashMap<String,Group>();
+
+
         @Override
         protected JRDataSource createDataSource(final Parameter _parameter)
             throws EFapsException
         {
             final DRDataSource dataSource = new DRDataSource("group", "category", "date", "amount");
-            final Map<String, Map<String, BigDecimal>> rowMap = new TreeMap<String, Map<String, BigDecimal>>();
+
             final Map<Instance, Instance> inst2inst = new HashMap<Instance, Instance>();
             final QueryBuilder queryBldr = new QueryBuilder(CISales.Payment);
             final MultiPrintQuery multi = queryBldr.getPrint();
@@ -165,42 +173,88 @@ public abstract class CashFlowReport_Base
                 final boolean out = multi2.getCurrentInstance().getType().isKindOf(
                                 CISales.TransactionOutbound.getType());
 
-                final String category = getCategory(_parameter, out, payInst, inst2inst.get(payInst));
-                final String dateStr = getDateStr(_parameter, date);
-                Map<String, BigDecimal> map;
-                if (rowMap.containsKey(category)) {
-                    map = rowMap.get(category);
-                } else {
-                    map = new HashMap<String, BigDecimal>();
-                    rowMap.put(category, map);
-                }
-                BigDecimal amountTmp;
-                if (map.containsKey(dateStr)) {
-                    amountTmp = map.get(dateStr);
-                } else {
-                    amountTmp = BigDecimal.ZERO;
-                }
                 if (out) {
                     amount = amount.negate();
                 }
-                map.put(dateStr, amountTmp.add(amount));
+                final Group group = getGroup(_parameter, out, payInst, inst2inst.get(payInst));
+
+                final String dateStr = getDateStr(_parameter, date);
+
+                group.add(_parameter,  out, payInst, inst2inst.get(payInst) , dateStr, amount);
             }
 
-            for (final Entry<String, Map<String, BigDecimal>> entryRow : rowMap.entrySet()) {
-                for (final Entry<String, BigDecimal> entryCol : entryRow.getValue().entrySet()) {
-                    final String prodName = entryCol.getKey();
-                    final BigDecimal quantity = entryCol.getValue();
-                    dataSource.add(entryRow.getKey(), prodName, quantity);
+            final Map<String, BigDecimal> saldos = new TreeMap<String, BigDecimal>();
+            for (final Group group : this.groups.values()) {
+                final Map<String, BigDecimal> saldo = group.getSaldo(_parameter);
+                for (final Entry<String, BigDecimal> entry : saldo.entrySet()) {
+                    BigDecimal amount;
+                    if (saldos.containsKey(entry.getKey())) {
+                        amount = saldos.get(entry.getKey());
+                    } else {
+                        amount = BigDecimal.ZERO;
+                    }
+                    saldos.put(entry.getKey(), amount.add(entry.getValue()));
                 }
             }
-
+            final Group open = new Group(0, "OPEN");
+            this.groups.put(open.getName(), open);
+            final Category cat = new Category(open, 0, "OPEN");
+            open.getCategories().put("OPEN", cat);
+            BigDecimal currentAmount = BigDecimal.ZERO;
+            for (final Entry<String, BigDecimal> saldo : saldos.entrySet()) {
+                    cat.add(_parameter, saldo.getKey(), currentAmount);
+                    currentAmount = currentAmount.add(saldo.getValue());
+            }
+            final List<Group> grpList = new ArrayList<Group>(this.groups.values());
+            Collections.sort(grpList, new Comparator<Group>()
+            {
+                @Override
+                public int compare(final Group _o1,
+                                   final Group _o2)
+                {
+                    return _o1.getWeight().compareTo(_o1.getWeight());
+                }
+            });
+            int i = 1;
+            for (final Group group : grpList) {
+                group.setNumber(i);
+                group.add2DataSource(_parameter, dataSource);
+                i++;
+            }
             return dataSource;
         }
 
+        /**
+         * @param _parameter
+         * @param _data
+         * @param _out
+         * @param _payInst
+         * @param _instance
+         * @return
+         */
+        protected Group getGroup(final Parameter _parameter,
+                                 final boolean _out,
+                                 final Instance _payInst,
+                                 final Instance _instance)
+        {
+            String key;
+            if (_out) {
+                key = "OUT";
+                if (!this.groups.containsKey(key)) {
+                    this.groups.put(key, new Group(2, key));
+                }
+            } else {
+                key = "IN";
+                if (!this.groups.containsKey(key)) {
+                    this.groups.put(key, new Group(1, key));
+                }
+            }
+            return this.groups.get(key);
+        }
 
         /**
          * @param _parameter Parameter as passed by the eFaps API
-         * @param _date      DateTime the String is wanted for
+         * @param _date DateTime the String is wanted for
          * @return String for Date
          * @throws EFapsException on error
          */
@@ -213,46 +267,12 @@ public abstract class CashFlowReport_Base
             if (props.containsKey("Pattern4Date")) {
                 pattern = (String) props.get("Pattern4Date");
             } else {
-                pattern = "MM/yyyy";
+                pattern = "MM - MMM - yyyy";
             }
             final DateTimeFormatter formatter = DateTimeFormat.forPattern(pattern)
                             .withLocale(Context.getThreadContext().getLocale());
 
             return _date.toString(formatter);
-        }
-
-
-        /**
-         * @param _parameter    Parameter as passed by the eFaps API
-         * @param _out          outgoing or not
-         * @param _payInst      Instance of the Payment
-         * @param _docInst      Instance of the Document
-         * @return name of the category
-         * @throws EFapsException on error
-         */
-        protected String getCategory(final Parameter _parameter,
-                                     final boolean _out,
-                                     final Instance _payInst,
-                                     final Instance _docInst)
-            throws EFapsException
-        {
-            String ret;
-            if (_docInst == null) {
-                ret = "4. Finanzas";
-            } else if (_docInst.isValid() && _docInst.getType().isKindOf(CISales.Invoice.getType())){
-                ret = "1. Venta";
-            } else if (_docInst.isValid() && _docInst.getType().isKindOf(CISales.IncomingInvoice.getType())){
-                ret = "2. Compra";
-            } else if (_docInst.isValid() && _docInst.getType().isKindOf(CISales.IncomingCredit.getType())){
-                ret = "5. Créditos";
-                //Payroll_Payslip
-            } else if (_docInst.isValid() && _docInst.getType().getUUID().equals(
-                            UUID.fromString("a298d361-7530-4a24-b69f-ff3a1186a081"))){
-                ret = "3. Sueldos";
-            } else {
-                ret = "6. Otros";
-            }
-            return ret;
         }
 
         @Override
@@ -277,5 +297,310 @@ public abstract class CashFlowReport_Base
             _builder.summary(crosstab);
         }
 
+
+        /**
+         * Getter method for the instance variable {@link #groups}.
+         *
+         * @return value of instance variable {@link #groups}
+         */
+        protected Map<String, Group> getGroups()
+        {
+            return this.groups;
+        }
+    }
+
+    public static class Group
+    {
+
+        private final String key;
+
+        private final Map<String, Category> categories = new HashMap<String, Category>();
+
+        private final int weight;
+
+        private Integer number = 0;
+
+        /**
+         * @param _groupName
+         */
+        public Group(final int _weight,
+                     final String _key)
+        {
+            this.weight = _weight;
+            this.key = _key;
+        }
+
+        /**
+         * @param _parameter
+         * @param _out
+         * @param _payInst
+         * @param _instance
+         * @param _dateStr
+         * @param _amount
+         */
+        public void add(final Parameter _parameter,
+                        final boolean _out,
+                        final Instance _payInst,
+                        final Instance _docInst,
+                        final String _dateStr,
+                        final BigDecimal _amount)
+        {
+            String key;
+            int weight = 1;
+            if (_docInst == null) {
+                key = "NONE";
+                weight = 10;
+            } else if (_docInst.isValid() && _docInst.getType().isKindOf(CISales.Invoice.getType())) {
+                key = "SELL";
+                weight = 1;
+            } else if (_docInst.isValid() && _docInst.getType().isKindOf(CISales.IncomingInvoice.getType())) {
+                key = "BUY";
+                weight = 2;
+            } else if (_docInst.isValid() && _docInst.getType().isKindOf(CISales.IncomingCredit.getType())) {
+                key = "CREDIT";
+                weight = 3;
+                // Payroll_Payslip
+            } else if (_docInst.isValid() && _docInst.getType().getUUID().equals(
+                            UUID.fromString("a298d361-7530-4a24-b69f-ff3a1186a081"))) {
+                key = "PAYROLL";
+                weight = 4;
+            } else {
+                key = "OTHER";
+                weight = 5;
+            }
+
+            Category category;
+            if (this.categories.containsKey(key)) {
+                category = this.categories.get(key);
+            } else {
+                category = new Category(this, weight,  key);
+                this.categories.put(category.getKey(), category);
+            }
+            category.add(_parameter, _dateStr, _amount);
+        }
+
+        /**
+         * @param _parameter
+         * @return
+         */
+        public Map<String, BigDecimal> getSaldo(final Parameter _parameter)
+        {
+            final Map<String, BigDecimal> ret = new TreeMap<String, BigDecimal>();
+            for (final Category category : this.categories.values()) {
+                final Map<String, BigDecimal> amounts = category.getAmounts();
+                for (final Entry<String, BigDecimal> entry : amounts.entrySet()) {
+                    BigDecimal amount;
+                    if (ret.containsKey(entry.getKey())) {
+                        amount = ret.get(entry.getKey());
+                    } else {
+                        amount = BigDecimal.ZERO;
+                    }
+                    ret.put(entry.getKey(), amount.add(entry.getValue()));
+                }
+            }
+            return ret;
+        }
+
+        /**
+         * @param _dataSource
+         */
+        public void add2DataSource(final Parameter _parameter,
+                                   final DRDataSource _dataSource)
+        {
+            final List<Category> catList = new ArrayList<Category>(this.categories.values());
+            Collections.sort(catList, new Comparator<Category>()
+            {
+                @Override
+                public int compare(final Category _o1,
+                                   final Category _o2)
+                {
+                    return _o1.getWeight().compareTo(_o1.getWeight());
+                }
+            });
+            int i = 1;
+            for (final Category category : catList) {
+                category.setNumber(i);
+                category.add2DataSource(_parameter, _dataSource);
+                i++;
+            }
+        }
+
+        /**
+         * Getter method for the instance variable {@link #name}.
+         *
+         * @return value of instance variable {@link #name}
+         */
+        public String getName()
+        {
+            return this.number + ". " + DBProperties.getProperty(CashFlowReport.class.getName() + ".Group." + getKey());
+        }
+
+        /**
+         * Getter method for the instance variable {@link #weight}.
+         *
+         * @return value of instance variable {@link #weight}
+         */
+        public Integer getWeight()
+        {
+            return this.weight;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #key}.
+         *
+         * @return value of instance variable {@link #key}
+         */
+        public String getKey()
+        {
+            return this.key;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #categories}.
+         *
+         * @return value of instance variable {@link #categories}
+         */
+        public Map<String, Category> getCategories()
+        {
+            return this.categories;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #number}.
+         *
+         * @return value of instance variable {@link #number}
+         */
+        public Integer getNumber()
+        {
+            return this.number;
+        }
+
+        /**
+         * Setter method for instance variable {@link #number}.
+         *
+         * @param _number value for instance variable {@link #number}
+         */
+        public void setNumber(final Integer _number)
+        {
+            this.number = _number;
+        }
+    }
+
+    public static class Category
+    {
+
+        private final String key;
+
+        private final Map<String, BigDecimal> amounts = new HashMap<String, BigDecimal>();
+
+        private final Group group;
+
+        private final int weight;
+
+        private Integer number;
+
+        /**
+         * @param _categoryName
+         */
+        public Category(final Group _group,
+                        final int _weight,
+                        final String _key)
+        {
+            this.weight = _weight;
+            this.group = _group;
+            this.key = _key;
+        }
+
+        /**
+         * @param _parameter
+         * @param _dataSource
+         */
+        public void add2DataSource(final Parameter _parameter,
+                                   final DRDataSource _dataSource)
+        {
+            for (final Entry<String, BigDecimal> entry : this.amounts.entrySet()) {
+                _dataSource.add(this.group.getName(), getName(), entry.getKey(), entry.getValue());
+            }
+        }
+
+        /**
+         * @param _parameter
+         * @param _dateStr
+         * @param _amount
+         */
+        public void add(final Parameter _parameter,
+                        final String _dateStr,
+                        final BigDecimal _amount)
+        {
+            BigDecimal amount;
+            if (this.amounts.containsKey(_dateStr)) {
+                amount = this.amounts.get(_dateStr);
+            } else {
+                amount = BigDecimal.ZERO;
+            }
+            this.amounts.put(_dateStr, amount.add(_amount));
+        }
+
+        /**
+         * Getter method for the instance variable {@link #name}.
+         *
+         * @return value of instance variable {@link #name}
+         */
+        public String getName()
+        {
+            return this.number + ". "
+                            + DBProperties.getProperty(CashFlowReport.class.getName() + ".Category."
+                                            + this.group.getKey() + "." + this.key);
+        }
+
+        /**
+         * Getter method for the instance variable {@link #amounts}.
+         *
+         * @return value of instance variable {@link #amounts}
+         */
+        public Map<String, BigDecimal> getAmounts()
+        {
+            return this.amounts;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #key}.
+         *
+         * @return value of instance variable {@link #key}
+         */
+        public String getKey()
+        {
+            return this.key;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #weight}.
+         *
+         * @return value of instance variable {@link #weight}
+         */
+        public Integer getWeight()
+        {
+            return this.weight;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #number}.
+         *
+         * @return value of instance variable {@link #number}
+         */
+        public Integer getNumber()
+        {
+            return this.number;
+        }
+
+        /**
+         * Setter method for instance variable {@link #number}.
+         *
+         * @param _number value for instance variable {@link #number}
+         */
+        public void setNumber(final Integer _number)
+        {
+            this.number = _number;
+        }
     }
 }
