7508363f199440422702576e828d35a91f1d4a55
jose.mallqui@efaps.org <jose.mallqui@efaps.org@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-01-16 23:05:14 +0000

    - sales: validate documents repeated on search IncomingPerceptionRequest
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@11732 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/IncomingPerceptionCertificate_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/IncomingPerceptionCertificate_Base.java
@@ -23,13 +23,23 @@ package org.efaps.esjp.sales.document;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import org.efaps.admin.datamodel.Status;
+import org.efaps.admin.dbproperty.DBProperties;
 import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Parameter.ParameterValues;
 import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Insert;
+import org.efaps.db.Instance;
+import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.PrintQuery;
+import org.efaps.db.QueryBuilder;
 import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.erp.NumberFormatter;
 import org.efaps.util.EFapsException;
@@ -60,7 +70,7 @@ public abstract class IncomingPerceptionCertificate_Base
                            final CreatedDoc _createdDoc)
         throws EFapsException
     {
-        @SuppressWarnings("unchecked") final BigDecimal perception = (BigDecimal) _createdDoc.getValue(PERCEPTIONVALUE);
+        final BigDecimal perception = (BigDecimal) _createdDoc.getValue(PERCEPTIONVALUE);
 
         final Insert insert = new Insert(CISales.IncomingPerceptionCertificate);
         insert.add(CISales.IncomingPerceptionCertificate.Date,
@@ -108,6 +118,111 @@ public abstract class IncomingPerceptionCertificate_Base
         relInsert.add(CISales.IncomingPerceptionCertificate2Document.FromLink, insert.getInstance());
         relInsert.add(CISales.IncomingPerceptionCertificate2Document.ToLink, _createdDoc.getInstance());
         relInsert.execute();
+    }
+
+    public Return validateConnectDocument(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Return ret = new Return();
+        final Map<?, ?> others = (HashMap<?, ?>) _parameter.get(ParameterValues.OTHERS);
+        final StringBuilder html = new StringBuilder();
+        final String[] childOids = (String[]) others.get("selectedRow");
+        boolean validate = true;
+
+        if (childOids != null) {
+            final Instance callInstance = _parameter.getCallInstance();
+            for (final String childOid : childOids) {
+                final Instance child = Instance.get(childOid);
+                if (callInstance.getType().isKindOf(CISales.IncomingPerceptionCertificate.getType())) {
+                    if (child.getType().equals(CISales.IncomingInvoice.getType())
+                                    && check4Relation(CISales.IncomingPerceptionCertificate2IncomingInvoice.uuid, child).next()) {
+                        validate = false;
+                        html.append(getString4ReturnInvalidate(child));
+                        break;
+                    }
+                }
+            }
+            if (validate) {
+                ret.put(ReturnValues.TRUE, true);
+                html.append(DBProperties.getProperty("org.efaps.esjp.sales.document.ValidateConnectDoc"));
+                ret.put(ReturnValues.SNIPLETT, html.toString());
+            } else {
+                html.insert(0, DBProperties.getProperty("org.efaps.esjp.sales.document.InvalidateConnectDoc")
+                                + "<p>");
+                ret.put(ReturnValues.SNIPLETT, html.toString());
+            }
+        }
+        return ret;
+    }
+
+    protected StringBuilder getString4ReturnInvalidate(final Instance _child)
+        throws EFapsException
+    {
+        final StringBuilder html = new StringBuilder();
+        final PrintQuery print = new PrintQuery(_child);
+        print.addAttribute(CISales.DocumentAbstract.Name);
+        print.execute();
+        return html.append(_child.getType().getLabel()
+                        + " - " + print.<String>getAttribute(CISales.DocumentAbstract.Name));
+    }
+
+    /**
+     * Check if the a relation of the given type already exists for this
+     * instance.
+     *
+     * @param _typeUUID uuid of the ration type
+     * @param _instance instance to be checked
+     * @return true if already an relation of the given type exists for the
+     *         instance, else false
+     * @throws EFapsException on error
+     */
+    protected MultiPrintQuery check4Relation(final UUID _typeUUID,
+                                             final Instance _instance)
+        throws EFapsException
+    {
+        final QueryBuilder queryBldr = new QueryBuilder(_typeUUID);
+        queryBldr.addWhereAttrMatchValue(CISales.IncomingPerceptionCertificate2Document.ToAbstractLink, _instance.getId());
+        final MultiPrintQuery multi = queryBldr.getPrint();
+        multi.addAttribute(CISales.IncomingPerceptionCertificate2Document.OID);
+        multi.execute();
+
+        return multi;
+    }
+
+    public Return connectDocument(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Map<?, ?> others = (HashMap<?, ?>) _parameter.get(ParameterValues.OTHERS);
+        final Map<?, ?> props = (HashMap<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
+        final String[] childOids = (String[]) others.get("selectedRow");
+        if (childOids != null) {
+            final Instance callInstance = _parameter.getCallInstance();
+            for (final String childOid : childOids) {
+                final Instance child = Instance.get(childOid);
+                Insert insert = null;
+                if (callInstance.getType().isKindOf(CISales.IncomingPerceptionCertificate.getType())) {
+                    // defaults
+                     if (child.getType().equals(CISales.IncomingInvoice.getType())) {
+                        insert = new Insert(CISales.IncomingPerceptionCertificate2IncomingInvoice);
+                    }
+                    int i = 0;
+                    while (insert == null && props.containsKey("connect" + i)) {
+                        final String connectUUIDStr = (String) props.get("connect" + i);
+                        final String[] connectUUIDs = connectUUIDStr.split(";");
+                        if (child.getType().getUUID().equals(UUID.fromString(connectUUIDs[0]))) {
+                            insert = new Insert(UUID.fromString(connectUUIDs[1]));
+                        }
+                        i++;
+                    }
+                }
+                if (insert != null) {
+                    insert.add(CISales.IncomingPerceptionCertificate2Document.FromLink, callInstance.getId());
+                    insert.add(CISales.IncomingPerceptionCertificate2Document.ToLink, child.getId());
+                    insert.execute();
+                }
+            }
+        }
+        return new Return();
     }
 
 }
