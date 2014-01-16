8680778047a3ff9cb0b748e0cde2a776a5d2c269
luis.moreyra@efaps.org <luis.moreyra@efaps.org@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-01-16 16:24:18 +0000

    - sales: adding reports  to credit note and reminder
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@11720 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/CreditNote_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/CreditNote_Base.java
@@ -20,10 +20,22 @@
 
 package org.efaps.esjp.sales.document;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.efaps.admin.dbproperty.DBProperties;
 import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Parameter.ParameterValues;
 import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Checkin;
+import org.efaps.esjp.ci.CISales;
+import org.efaps.esjp.common.jasperreport.StandartReport;
 import org.efaps.util.EFapsException;
 
 /**
@@ -48,9 +60,31 @@ public abstract class CreditNote_Base
     public Return create(final Parameter _parameter)
         throws EFapsException
     {
+        Return ret = new Return();
         final CreatedDoc createdDoc = createDoc(_parameter);
         createPositions(_parameter, createdDoc);
         connect2Derived(_parameter, createdDoc);
-        return new Return();
+
+        final Map<?, ?> properties = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
+        if (properties.containsKey("JasperReport")) {
+            final StandartReport report = new StandartReport();
+            _parameter.put(ParameterValues.INSTANCE, createdDoc.getInstance());
+
+            final String fileName = DBProperties.getProperty("Sales_CreditNote.Label", "es") + "_"
+                            + createdDoc.getValues().get(CISales.DocumentStockAbstract.Name.name);
+            report.setFileName(fileName);
+            ret = report.execute(_parameter);
+            ret.put(ReturnValues.TRUE, true);
+
+            try {
+                final File file = (File) ret.get(ReturnValues.VALUES);
+                final InputStream input = new FileInputStream(file);
+                final Checkin checkin = new Checkin(createdDoc.getInstance());
+                checkin.execute(fileName + "." + properties.get("Mime"), input, ((Long) file.length()).intValue());
+            } catch (final FileNotFoundException e) {
+                throw new EFapsException(Invoice.class, "create.FileNotFoundException", e);
+            }
+        }
+        return ret;
     }
 }
