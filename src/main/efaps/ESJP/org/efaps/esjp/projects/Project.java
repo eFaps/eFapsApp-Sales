e511bef91aba272fdab03885b18c2014b423d799
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-04-30 20:12:33 +0000

    - sales: adding profile for projects
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@12583 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/projects/Project.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.projects;
+
+import org.efaps.admin.program.esjp.EFapsNoUpdate;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+
+/**
+ * This class is only used in case that the Projects App is not installed
+ * to be able to compile the classes and to enable linking during first
+ * install.
+ * @author The eFaps Team
+ */
+@EFapsUUID("7bc8c88f-64c3-402d-aed9-1ad0d93f5437")
+@EFapsRevision("$Rev$")
+@EFapsNoUpdate
+public class Project
+{
+
+}
