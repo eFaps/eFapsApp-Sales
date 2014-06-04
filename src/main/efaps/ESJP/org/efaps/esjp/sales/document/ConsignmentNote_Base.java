dde378a1e77a1be35670bcdcdab4db8ab16c502b
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-06-04 01:59:55 +0000

    - sales: adding basic configurationitems for consignmentnote
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@12970 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/ConsignmentNote_Base.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.sales.document;
+
+import java.io.File;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Return.ReturnValues;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.util.EFapsException;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("98066068-5423-4f7e-a8e8-d90574dc5b82")
+@EFapsRevision("$Rev$")
+public abstract class ConsignmentNote_Base
+    extends AbstractProductDocument
+{
+
+    /**
+     * @param _parameter Parameter as passed from the eFaps API.
+     * @return new Return.
+     * @throws EFapsException on error.
+     */
+    public Return create(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Return ret = new Return();
+        final CreatedDoc doc = createDoc(_parameter);
+        createPositions(_parameter, doc);
+        connect2ProductDocumentType(_parameter, doc);
+        connect2Derived(_parameter, doc);
+        connect2Object(_parameter, doc);
+
+        final File file = createReport(_parameter, doc);
+        if (file != null) {
+            ret.put(ReturnValues.VALUES, file);
+            ret.put(ReturnValues.TRUE, true);
+        }
+        return ret;
+    }
+}
