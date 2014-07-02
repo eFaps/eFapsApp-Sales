6e5243c045f05b677b529d7b902d3a6b126f548f
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-07-02 16:10:39 +0000

    - sales: correction of esjp for reservation
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@13221 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/Reservation_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/Reservation_Base.java
@@ -22,14 +22,10 @@ package org.efaps.esjp.sales.document;
 
 import java.io.File;
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang3.StringEscapeUtils;
-import org.efaps.admin.datamodel.Dimension;
 import org.efaps.admin.dbproperty.DBProperties;
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Parameter.ParameterValues;
@@ -37,20 +33,17 @@ import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
-import org.efaps.db.AttributeQuery;
 import org.efaps.db.Context;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
 import org.efaps.db.InstanceQuery;
 import org.efaps.db.MultiPrintQuery;
-import org.efaps.db.PrintQuery;
 import org.efaps.db.QueryBuilder;
 import org.efaps.db.SelectBuilder;
 import org.efaps.esjp.admin.datamodel.StatusValue;
 import org.efaps.esjp.ci.CIProducts;
 import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.ci.CITableSales;
-import org.efaps.esjp.erp.NumberFormatter;
 import org.efaps.esjp.products.util.Products;
 import org.efaps.esjp.products.util.ProductsSettings;
 import org.efaps.util.EFapsException;
@@ -228,116 +221,20 @@ public abstract class Reservation_Base
         return ret;
     }
 
+
     @Override
-    public Return updateFields4Product(final Parameter _parameter)
+    protected void add2UpdateField4Product(final Parameter _parameter,
+                                           final Map<String, Object> _map,
+                                           final Instance _prodInst)
         throws EFapsException
     {
-        final Return retVal = new Return();
-        final DecimalFormat formater = NumberFormatter.get().getFrmt4Quantity(getTypeName4SysConf(_parameter));
-        final Instance defaultStorageInst = Products.getSysConfig().getLink(ProductsSettings.DEFAULTWAREHOUSE);
-        final List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-        final Map<String, Object> map = new HashMap<String, Object>();
-
-        final int selected = getSelectedRow(_parameter);
-        final String oid = _parameter.getParameterValues("product")[selected];
-        final Instance instProd = Instance.get(oid);
-        // validate that a product was selected
-        if (instProd.isValid()) {
-            long selectedUoM;
-            Long dimId;
-            Long dUoMId;
-            final QueryBuilder queryBldr = new QueryBuilder(CIProducts.Inventory);
-            queryBldr.addWhereAttrEqValue(CIProducts.Inventory.Product, instProd);
-            if (defaultStorageInst.isValid()) {
-                queryBldr.addWhereAttrEqValue(CIProducts.Inventory.Storage, defaultStorageInst);
-            } else {
-                final QueryBuilder storeBldr = new QueryBuilder(CIProducts.Warehouse);
-                final AttributeQuery storequery = storeBldr.getAttributeQuery(CIProducts.Warehouse.ID);
-                queryBldr.addWhereAttrInQuery(CIProducts.Inventory.Storage, storequery);
+        super.add2UpdateField4Product(_parameter, _map, _prodInst);
+        if (!_map.containsKey(CITableSales.Sales_DeliveryNotePositionTable.quantityInStock.name)) {
+            final Instance defaultStorageInst = Products.getSysConfig().getLink(ProductsSettings.DEFAULTWAREHOUSE);
+            if (defaultStorageInst != null && defaultStorageInst.isValid()) {
+                _map.put(CITableSales.Sales_DeliveryNotePositionTable.quantityInStock.name,
+                        getStock4ProductInStorage(_parameter, _prodInst, defaultStorageInst));
             }
-            final MultiPrintQuery multi = queryBldr.getPrint();
-            final SelectBuilder selDesc = SelectBuilder.get().linkto(CIProducts.Inventory.Product)
-                            .attribute(CIProducts.ProductAbstract.Description);
-            final SelectBuilder selDim = SelectBuilder.get().linkto(CIProducts.Inventory.Product)
-                            .attribute(CIProducts.ProductAbstract.Dimension);
-            final SelectBuilder selUoM = SelectBuilder.get().linkto(CIProducts.Inventory.Product)
-                            .attribute(CIProducts.ProductAbstract.DefaultUoM);
-            multi.addSelect(selDesc, selDim, selUoM);
-            multi.addAttribute(CIProducts.Inventory.Quantity);
-            multi.execute();
-            if (multi.next()) {
-                final BigDecimal quantity = multi.<BigDecimal>getAttribute(CIProducts.Inventory.Quantity);
-                dimId = multi.<Long>getSelect(selDim);
-                dUoMId = multi.<Long>getSelect(selUoM);
-                map.put(CITableSales.Sales_ReservationPositionTable.quantity.name,
-                                BigDecimal.ONE.toString());
-                map.put(CITableSales.Sales_ReservationPositionTable.quantityInStock.name,
-                                formater.format(quantity));
-                map.put(CITableSales.Sales_ReservationPositionTable.productDesc.name,
-                                StringEscapeUtils.escapeEcmaScript(multi.<String>getSelect(selDesc)));
-            } else {
-                final PrintQuery print = new PrintQuery(instProd);
-                print.addAttribute(CIProducts.ProductAbstract.Description,
-                                CIProducts.ProductAbstract.Dimension,
-                                CIProducts.ProductAbstract.DefaultUoM);
-                print.execute();
-                dimId = print.getAttribute(CIProducts.ProductAbstract.Dimension);
-                dUoMId = print.getAttribute(CIProducts.ProductAbstract.DefaultUoM);
-                map.put(CITableSales.Sales_ReservationPositionTable.quantity.name,
-                                BigDecimal.ONE.toString());
-                map.put(CITableSales.Sales_ReservationPositionTable.quantityInStock.name,
-                                formater.format(BigDecimal.ZERO));
-                map.put(CITableSales.Sales_ReservationPositionTable.productDesc.name,
-                                StringEscapeUtils.escapeEcmaScript(print.<String>getAttribute(CIProducts.ProductAbstract.Description)));
-            }
-            if (dUoMId == null) {
-                selectedUoM = Dimension.get(dimId).getBaseUoM().getId();
-            } else {
-                if (Dimension.getUoM(dUoMId).getDimension().equals(Dimension.get(dimId))) {
-                    selectedUoM = dUoMId;
-                } else {
-                    selectedUoM = Dimension.get(dimId).getBaseUoM().getId();
-                }
-            }
-            map.put(CITableSales.Sales_ReceiptPositionTable.uoM.name, getUoMFieldStr(selectedUoM, dimId));
-            list.add(map);
-            retVal.put(ReturnValues.VALUES, list);
-        } else {
-            list.add(map);
-            retVal.put(ReturnValues.VALUES, list);
         }
-        return retVal;
     }
-
-    protected String getStockProduct4Storage(final Parameter _parameter,
-                                             final Instance productinst,
-                                             final Instance storageInst)
-        throws EFapsException
-    {
-        String ret = "";
-        final DecimalFormat qtyFrmt = NumberFormatter.get().getFrmt4Quantity(getTypeName4SysConf(_parameter));
-
-
-        final QueryBuilder queryBldr = new QueryBuilder(CIProducts.Inventory);
-        if (storageInst.isValid()) {
-            queryBldr.addWhereAttrEqValue(CIProducts.Inventory.Storage, storageInst);
-        } else {
-            final QueryBuilder storeBldr = new QueryBuilder(CIProducts.Warehouse);
-            final AttributeQuery storequery = storeBldr.getAttributeQuery(CIProducts.Warehouse.ID);
-            queryBldr.addWhereAttrInQuery(CIProducts.Inventory.Storage, storequery);
-        }
-        queryBldr.addWhereAttrEqValue(CIProducts.Inventory.Product, productinst);
-        final MultiPrintQuery multi = queryBldr.getPrint();
-        multi.addAttribute(CIProducts.Inventory.Quantity, CIProducts.Inventory.Reserved);
-        multi.execute();
-        if (multi.next()) {
-            final BigDecimal quantity = multi.getAttribute(CIProducts.Inventory.Quantity);
-            ret = qtyFrmt.format(quantity);
-        } else {
-            ret = "0";
-        }
-
-        return ret;
-    }
-
 }
