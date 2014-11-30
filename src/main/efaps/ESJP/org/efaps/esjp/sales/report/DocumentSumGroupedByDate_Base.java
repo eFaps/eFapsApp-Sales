ff97a5fb6c7c8f989a245020e1a3d33bed80b713
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2014-11-30 22:44:37 +0000

    - sales: adding report for document sums
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/sales/trunk@14547 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/report/DocumentSumGroupedByDate_Base.java
@@ -0,0 +1,280 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.sales.report;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.efaps.admin.datamodel.Type;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Instance;
+import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.QueryBuilder;
+import org.efaps.db.SelectBuilder;
+import org.efaps.esjp.ci.CISales;
+import org.efaps.esjp.common.AbstractCommon;
+import org.efaps.esjp.erp.CurrencyInst;
+import org.efaps.util.EFapsException;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DurationFieldType;
+import org.joda.time.Partial;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("e7448d39-68b9-45b3-b63b-f9a911040358")
+@EFapsRevision("$Rev$")
+public abstract class DocumentSumGroupedByDate_Base
+    extends AbstractCommon
+{
+
+    public enum DateGroup
+    {
+        WEEK(DurationFieldType.weeks()),
+
+        MONTH(DurationFieldType.months());
+
+        private final DurationFieldType fieldType;
+
+        private DateGroup(final DurationFieldType _fieldType)
+        {
+            this.fieldType = _fieldType;
+        }
+
+        public DurationFieldType getFieldType()
+        {
+            return this.fieldType;
+        }
+
+    }
+
+    public Partial getPartial(final DateTime _date,
+                              final DateGroup _dateGourp)
+    {
+        return getPartial(_date, _dateGourp.getFieldType());
+    }
+
+    public Partial getPartial(final DateTime _date,
+                              final DurationFieldType _fieldType)
+    {
+        Partial ret = null;
+        if (DurationFieldType.weeks().equals(_fieldType)) {
+            ret = new Partial(new DateTimeFieldType[] { DateTimeFieldType.weekyear(),
+                            DateTimeFieldType.weekOfWeekyear() },
+                            new int[] { _date.getWeekyear(), _date.getWeekOfWeekyear() });
+        } else if (DurationFieldType.months().equals(_fieldType)) {
+            ret = new Partial(new DateTimeFieldType[] { DateTimeFieldType.year(),
+                            DateTimeFieldType.monthOfYear() },
+                            new int[] { _date.getYear(), _date.getMonthOfYear() });
+        }
+        return ret;
+    }
+
+    public List<? extends DataBean> getDataBeans(final DateTime _start,
+                                                 final DateTime _end,
+                                                 final DateGroup _dateGourp,
+                                                 final Type... _types)
+        throws EFapsException
+    {
+
+        final Map<Partial, DataBean> dateGroupmap = new HashMap<>();
+        Partial startPartial = getPartial(_start, _dateGourp);
+        final Partial endPartial = getPartial(_end, _dateGourp);
+
+        while (!startPartial.isAfter(endPartial)) {
+            final DataBean bean = new DataBean().setPartial(startPartial);
+            dateGroupmap.put(startPartial, bean);
+            startPartial = startPartial.withFieldAdded(_dateGourp.getFieldType(), 1);
+        }
+
+        final QueryBuilder queryBldr;
+        if (_types == null || _types.length == 0) {
+            queryBldr = new QueryBuilder(CISales.DocumentSumAbstract);
+        } else {
+            queryBldr = new QueryBuilder(_types[0]);
+            for (int i = 1; i < _types.length; i++) {
+                queryBldr.addType(_types[i]);
+            }
+        }
+        queryBldr.addWhereAttrGreaterValue(CISales.DocumentSumAbstract.Date, _start.withTimeAtStartOfDay()
+                        .minusMinutes(1));
+        queryBldr.addWhereAttrLessValue(CISales.DocumentSumAbstract.Date, _end.withTimeAtStartOfDay().plusDays(1));
+        final MultiPrintQuery multi = queryBldr.getPrint();
+        final SelectBuilder selRateCurInst = SelectBuilder.get().linkto(CISales.DocumentSumAbstract.RateCurrencyId)
+                        .instance();
+        multi.addSelect(selRateCurInst);
+        multi.addAttribute(CISales.DocumentSumAbstract.CrossTotal,
+                        CISales.DocumentSumAbstract.RateCrossTotal,
+                        CISales.DocumentSumAbstract.Date);
+        multi.execute();
+        while (multi.next()) {
+            final DateTime dateTime = multi.getAttribute(CISales.DocumentSumAbstract.Date);
+            final BigDecimal crossTotal = multi.getAttribute(CISales.DocumentSumAbstract.CrossTotal);
+            final BigDecimal rateCrossTotal = multi.getAttribute(CISales.DocumentSumAbstract.RateCrossTotal);
+            final Instance rateCurInst = multi.getSelect(selRateCurInst);
+            final DataBean bean = dateGroupmap.get(getPartial(dateTime, _dateGourp));
+            bean.addAmount(multi.getCurrentInstance().getType(), "BASE", crossTotal);
+            bean.addAmount(multi.getCurrentInstance().getType(), CurrencyInst.get(rateCurInst).getISOCode(),
+                            rateCrossTotal);
+        }
+        return new ArrayList<>(dateGroupmap.values());
+    }
+
+    public static class ValueBean
+    {
+
+        private final Map<String, BigDecimal> amounts = new HashMap<>();
+
+        private Type type;
+
+        public ValueBean addAmount(final String _currISO,
+                                   final BigDecimal _amount)
+        {
+            BigDecimal amount;
+            if (this.amounts.containsKey(_currISO)) {
+                amount = this.amounts.get(_currISO);
+            } else {
+                amount = BigDecimal.ZERO;
+            }
+            this.amounts.put(_currISO, amount.add(_amount));
+            return this;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #type}.
+         *
+         * @return value of instance variable {@link #type}
+         */
+        public Type getType()
+        {
+            return this.type;
+        }
+
+        /**
+         * Setter method for instance variable {@link #type}.
+         *
+         * @param _type value for instance variable {@link #type}
+         */
+        public ValueBean setType(final Type _type)
+        {
+            this.type = _type;
+            return this;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #amounts}.
+         *
+         * @return value of instance variable {@link #amounts}
+         */
+        public Map<String, BigDecimal> getAmounts()
+        {
+            return this.amounts;
+        }
+    }
+
+    public static class DataBean
+    {
+
+        private Partial partial = new Partial();
+
+        private final Map<Type, ValueBean> values = new HashMap<>();
+
+        public DataBean addAmount(final Type _type,
+                                  final String _currISO,
+                                  final BigDecimal _amount)
+        {
+            ValueBean value;
+            if (this.values.containsKey(_type)) {
+                value = this.values.get(_type);
+            } else {
+                value = new ValueBean().setType(_type);
+                this.values.put(_type, value);
+            }
+            value.addAmount(_currISO, _amount);
+            return this;
+        }
+
+        /**
+         * Getter method for the instance variable {@link #partial}.
+         *
+         * @return value of instance variable {@link #partial}
+         */
+        public Partial getPartial()
+        {
+            return this.partial;
+        }
+
+        /**
+         * Setter method for instance variable {@link #partial}.
+         *
+         * @param _partial value for instance variable {@link #partial}
+         */
+        public DataBean setPartial(final Partial _partial)
+        {
+            this.partial = _partial;
+            return this;
+        }
+
+        @Override
+        public String toString()
+        {
+            return ToStringBuilder.reflectionToString(this);
+        }
+
+        /**
+         * @param _datasource
+         */
+        public void add2MapCollection(final Collection<Map<String, ?>> _datasource)
+            throws EFapsException
+        {
+            for (final ValueBean value : getValues().values()) {
+                final Map<String, Object> map = new HashMap<>();
+                _datasource.add(map);
+                map.put("group", getPartial().toString());
+                map.put("type", value.getType().getLabel());
+                for (final Entry<String, BigDecimal> entry : value.getAmounts().entrySet()) {
+                    map.put(entry.getKey(), entry.getValue());
+                }
+            }
+        }
+
+        /**
+         * Getter method for the instance variable {@link #values}.
+         *
+         * @return value of instance variable {@link #values}
+         */
+        public Map<Type, ValueBean> getValues()
+        {
+            return this.values;
+        }
+    }
+}
---
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/report/DocumentSumReport_Base.java
@@ -0,0 +1,319 @@
+/*
+ * Copyright 2003 - 2014 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.sales.report;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
+import net.sf.dynamicreports.report.builder.DynamicReports;
+import net.sf.dynamicreports.report.builder.crosstab.CrosstabBuilder;
+import net.sf.dynamicreports.report.builder.crosstab.CrosstabColumnGroupBuilder;
+import net.sf.dynamicreports.report.builder.crosstab.CrosstabMeasureBuilder;
+import net.sf.dynamicreports.report.builder.crosstab.CrosstabRowGroupBuilder;
+import net.sf.dynamicreports.report.constant.Calculation;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.data.JRMapCollectionDataSource;
+
+import org.efaps.admin.datamodel.Type;
+import org.efaps.admin.dbproperty.DBProperties;
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Parameter.ParameterValues;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.event.Return.ReturnValues;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.esjp.common.jasperreport.AbstractDynamicReport;
+import org.efaps.esjp.erp.Currency;
+import org.efaps.esjp.erp.CurrencyInst;
+import org.efaps.esjp.erp.FilteredReport;
+import org.efaps.esjp.erp.NumberFormatter;
+import org.efaps.esjp.sales.report.DocumentSumGroupedByDate_Base.DataBean;
+import org.efaps.esjp.sales.report.DocumentSumGroupedByDate_Base.ValueBean;
+import org.efaps.esjp.ui.html.dojo.charting.Axis;
+import org.efaps.esjp.ui.html.dojo.charting.ColumnsChart;
+import org.efaps.esjp.ui.html.dojo.charting.Data;
+import org.efaps.esjp.ui.html.dojo.charting.Orientation;
+import org.efaps.esjp.ui.html.dojo.charting.PlotLayout;
+import org.efaps.esjp.ui.html.dojo.charting.Serie;
+import org.efaps.esjp.ui.html.dojo.charting.Util;
+import org.efaps.util.EFapsException;
+import org.joda.time.DateTime;
+import org.joda.time.Partial;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("19c0ee49-3942-4872-9e7d-de1f0d73b446")
+@EFapsRevision("$Rev$")
+public abstract class DocumentSumReport_Base
+    extends FilteredReport
+{
+
+    /**
+     * Logging instance used in this class.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(DocumentSumReport.class);
+    private List<? extends DataBean> data;
+
+    /**
+     * @param _parameter Parameter as passed by the eFasp API
+     * @return Return containing html snipplet
+     * @throws EFapsException on error
+     */
+    public Return generateReport(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Return ret = new Return();
+        final AbstractDynamicReport dyRp = getReport(_parameter);
+        String html = dyRp.getHtmlSnipplet(_parameter);
+
+        final List<? extends DataBean> dataBeans = getData(_parameter);
+        Collections.sort(this.data, new Comparator<DataBean>()
+        {
+
+            @Override
+            public int compare(final DataBean _arg0,
+                               final DataBean _arg1)
+            {
+                return _arg0.getPartial().compareTo(_arg1.getPartial());
+            }
+        });
+        int x = 0;
+        final Map<Partial, Integer> xmap = new LinkedHashMap<>();
+
+        final Map<Type, ColumnsChart> carts = new HashMap<>();
+        final Map<Type, Map<String, Serie<Data>>> seriesMap = new HashMap<>();
+        final Axis axis = new Axis().setName("x");
+
+        for (final DataBean bean : dataBeans) {
+
+            if (!xmap.containsKey(bean.getPartial())) {
+                xmap.put(bean.getPartial(), x++);
+            }
+            for (final ValueBean value : bean.getValues().values()) {
+                final ColumnsChart columsChart;
+                final Map<String, Serie<Data>> series;
+                if (carts.containsKey(value.getType())) {
+                    columsChart = carts.get(value.getType());
+                    series = seriesMap.get(value.getType());
+                } else {
+                    series = new HashMap<>();
+                    columsChart = new ColumnsChart().setPlotLayout(PlotLayout.CLUSTERED).setGap(5);
+                    columsChart.setTitle(value.getType().getLabel());
+                    columsChart.setOrientation(Orientation.VERTICAL_CHART_LEGEND);
+
+                    for (final CurrencyInst currency : CurrencyInst.getAvailable()) {
+                        final Serie<Data> serie = new Serie<Data>();
+                        serie.setName(currency.getName());
+                        series.put(currency.getISOCode(), serie);
+                        columsChart.addSerie(serie);
+                    }
+                    final Serie<Data> baseSerie = new Serie<Data>();
+                    series.put("BASE", baseSerie);
+                    baseSerie.setName("BASE");
+                    columsChart.addSerie(baseSerie);
+                    columsChart.addAxis(axis);
+                    carts.put(value.getType(), columsChart);
+                    seriesMap.put(value.getType(), series);
+                }
+
+                for (final Entry<String, BigDecimal> entry : value.getAmounts().entrySet()) {
+                    final DecimalFormat fmtr = NumberFormatter.get().getFormatter();
+                    final Data data = new Data().setSimple(false);
+                    final Serie<Data> serie = series.get(entry.getKey());
+                    serie.addData(data);
+                    final BigDecimal y = value.getAmounts().get(entry.getKey());
+                    data.setXValue(xmap.get(bean.getPartial()));
+                    data.setYValue(y);
+                    data.setTooltip(fmtr.format(y) + " " + serie.getName() + " - " + bean.getPartial());
+                }
+            }
+        }
+        final List<Map<String, Object>> labels = new ArrayList<>();
+        for (final Entry<Partial, Integer> entry : xmap.entrySet()) {
+            final Map<String, Object> map = new HashMap<>();
+            map.put("value", entry.getValue());
+            map.put("text", Util.wrap4String(entry.getKey()));
+            labels.add(map);
+        }
+        axis.setLabels(Util.mapCollectionToObjectArray(labels));
+        for (final ColumnsChart chart : carts.values()) {
+            html = html + "<div style=\"float:left\">" + chart.getHtmlSnipplet() + "</div>";
+        }
+        ret.put(ReturnValues.SNIPLETT, html);
+        return ret;
+    }
+
+
+
+    /**
+     * @param _parameter Parameter as passed by the eFasp API
+     * @return Return containing the file
+     * @throws EFapsException on error
+     */
+    public Return exportReport(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Return ret = new Return();
+        final Map<?, ?> props = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
+        final String mime = (String) props.get("Mime");
+        final AbstractDynamicReport dyRp = getReport(_parameter);
+        dyRp.setFileName(DBProperties.getProperty(DocumentSumReport.class.getName() + ".FileName"));
+        File file = null;
+        if ("xls".equalsIgnoreCase(mime)) {
+            file = dyRp.getExcel(_parameter);
+        } else if ("pdf".equalsIgnoreCase(mime)) {
+            file = dyRp.getPDF(_parameter);
+        }
+        ret.put(ReturnValues.VALUES, file);
+        ret.put(ReturnValues.TRUE, true);
+        return ret;
+    }
+
+    protected List<? extends DataBean> getData(final Parameter _parameter)
+        throws EFapsException
+    {
+        if (this.data == null) {
+            final DocumentSumGroupedByDate ds = new DocumentSumGroupedByDate();
+            final Map<String, Object> filter = getFilterMap(_parameter);
+            final DateTime start;
+            final DateTime end;
+            if (filter.containsKey("dateFrom")) {
+                start = (DateTime) filter.get("dateFrom");
+            } else {
+                start = new DateTime();
+            }
+            if (filter.containsKey("dateTo")) {
+                end = (DateTime) filter.get("dateTo");
+            } else {
+                end = new DateTime();
+            }
+            this.data = ds.getDataBeans(start, end, DocumentSumGroupedByDate_Base.DateGroup.MONTH);
+        }
+        return this.data;
+    }
+
+    /**
+     * @param _parameter Parameter as passed by the eFasp API
+     * @return the report class
+     * @throws EFapsException on error
+     */
+    protected AbstractDynamicReport getReport(final Parameter _parameter)
+        throws EFapsException
+    {
+        return new DynDocumentSumReport(this);
+    }
+
+    public static class DynDocumentSumReport
+        extends AbstractDynamicReport
+    {
+
+        private final DocumentSumReport_Base sumReport;
+
+        /**
+         * @param _documentSumReport_Base
+         */
+        public DynDocumentSumReport(final DocumentSumReport_Base _sumReport)
+        {
+            this.sumReport = _sumReport;
+        }
+
+        @Override
+        protected JRDataSource createDataSource(final Parameter _parameter)
+            throws EFapsException
+        {
+            final List<? extends DataBean> data = this.sumReport.getData(_parameter);
+            Collections.sort(data, new Comparator<DataBean>()
+            {
+
+                @Override
+                public int compare(final DataBean _arg0,
+                                   final DataBean _arg1)
+                {
+                    return _arg0.getPartial().compareTo(_arg1.getPartial());
+                }
+            });
+            final Collection<Map<String, ?>> datasource = new ArrayList<>();
+            for (final DataBean bean : data) {
+                bean.add2MapCollection(datasource);
+            }
+            return new JRMapCollectionDataSource(datasource);
+        }
+
+        @Override
+        protected void addColumnDefintion(final Parameter _parameter,
+                                          final JasperReportBuilder _builder)
+            throws EFapsException
+        {
+            final CrosstabBuilder crosstab = DynamicReports.ctab.crosstab();
+
+            for (final CurrencyInst currency : CurrencyInst.getAvailable()) {
+                final CrosstabMeasureBuilder<BigDecimal> amountMeasure = DynamicReports.ctab.measure(
+                                currency.getSymbol(),
+                                currency.getISOCode(), BigDecimal.class, Calculation.SUM);
+                crosstab.addMeasure(amountMeasure);
+            }
+
+            final CrosstabMeasureBuilder<BigDecimal> amountMeasure = DynamicReports.ctab.measure(
+                            CurrencyInst.get(Currency.getBaseCurrency()).getSymbol(),
+                            "BASE", BigDecimal.class, Calculation.SUM);
+            crosstab.addMeasure(amountMeasure);
+
+            final CrosstabRowGroupBuilder<String> rowTypeGroup = DynamicReports.ctab.rowGroup("type", String.class)
+                            .setHeaderWidth(150);
+            crosstab.addRowGroup(rowTypeGroup);
+
+            final CrosstabColumnGroupBuilder<String> columnGroup = DynamicReports.ctab.columnGroup("group",
+                            String.class);
+
+            crosstab.addColumnGroup(columnGroup);
+
+            _builder.addSummary(crosstab);
+
+            /*
+             * final BarChartBuilder chart = DynamicReports.cht.barChart()
+             * .setCategory(groupColumn)
+             * .series(DynamicReports.cht.serie(amountColumn
+             * ).setSeries(typeColumn)) .setSeriesOrderType(OrderType.ASCENDING)
+             * .
+             * setValueAxisFormat(DynamicReports.cht.axisFormat().setLabel("Stock"
+             * )); _builder.addSummary(chart);
+             */
+        }
+    }
+
+}
