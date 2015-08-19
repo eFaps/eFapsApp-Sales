711a61f22d05e43f9cc042f8ce1e734f7dc24618
Jan Moxter <jan.moxter@innobix.com>
2015-08-18 19:40:05 -0500

    - Issue #29: Add a command to create an invoice in the table for DeliveryNotes
    closes #29

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/Invoice_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/Invoice_Base.java
@@ -74,6 +74,7 @@ public abstract class Invoice_Base
             ret.put(ReturnValues.VALUES, file);
             ret.put(ReturnValues.TRUE, true);
         }
+        ret.put(ReturnValues.INSTANCE, createdDoc.getInstance());
         return ret;
     }
 
