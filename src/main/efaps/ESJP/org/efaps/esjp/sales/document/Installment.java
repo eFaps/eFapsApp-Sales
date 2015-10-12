c55adf863970f73f0234edf632a5efebb211275c
Jan Moxter <jan.moxter@innobix.com>
2015-10-12 15:17:42 -0500

    - Issue #69: Add installments as document
    closes #69

---
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/Installment.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2003 - 2015 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.efaps.esjp.sales.document;
+
+import org.efaps.admin.program.esjp.EFapsApplication;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+/**
+ * This class must be replaced for customization, therefore it is left empty.
+ * Functional description can be found in the related "<code>_base</code>"
+ * class.
+ *
+ * @author The eFaps Team
+ */
+@EFapsUUID("c1e7d39a-7a3a-459b-abde-7fbbf6fd7c40")
+@EFapsApplication("eFapsApp-Sales")
+public class Installment
+    extends Installment_Base
+{
+
+}
