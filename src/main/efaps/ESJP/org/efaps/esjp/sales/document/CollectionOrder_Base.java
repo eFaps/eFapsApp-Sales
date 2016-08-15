3ab7beaf4fa4e759730d0d87178d4c7ba2eecd51
Jan Moxter <jan.moxter@innobix.com>
2016-08-15 12:59:20 -0500

    - Issue #282: Update CollectionOrder
    closes #282

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/CollectionOrder_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/CollectionOrder_Base.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003 - 2012 The eFaps Team
+ * Copyright 2003 - 2016 The eFaps Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,31 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Revision:        $Rev: 8180 $
- * Last Changed:    $Date: 2012-11-08 19:00:37 -0500 (jue, 08 nov 2012) $
- * Last Changed By: $Author: m.aranya@moxter.net $
  */
 
 package org.efaps.esjp.sales.document;
+
+import java.io.File;
 
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsApplication;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.ci.CIType;
+import org.efaps.esjp.ci.CISales;
 import org.efaps.util.EFapsException;
 
 /**
  * TODO comment!
  *
  * @author The eFaps Team
- * @version $Id: Payment_Base.java 7671 2012-06-14 17:25:53Z
- *          jorge.cueva@moxter.net $
  */
 @EFapsUUID("771297ac-8d56-42f6-a1ed-fa573b42698e")
 @EFapsApplication("eFapsApp-Sales")
 public abstract class CollectionOrder_Base
-    extends AbstractDocumentSum
+    extends AbstractSumOnlyDocument
 {
 
     /**
@@ -48,12 +47,46 @@ public abstract class CollectionOrder_Base
     public Return create(final Parameter _parameter)
         throws EFapsException
     {
-        final CreatedDoc createdDoc = createDoc(_parameter);
-        connect2Object(_parameter, createdDoc);
         final Return ret = new Return();
+        final CreatedDoc createdDoc = createDoc(_parameter);
+        connect2Derived(_parameter, createdDoc);
+        connect2Object(_parameter, createdDoc);
+
+        final File file = createReport(_parameter, createdDoc);
+        if (file != null) {
+            ret.put(ReturnValues.VALUES, file);
+            ret.put(ReturnValues.TRUE, true);
+        }
         ret.put(ReturnValues.INSTANCE, createdDoc.getInstance());
         return ret;
     }
 
+    /**
+     * Edit.
+     *
+     * @param _parameter Parameter from the eFaps API.
+     * @return new Return.
+     * @throws EFapsException on error.
+     */
+    public Return edit(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Return ret = new Return();
+        final EditedDoc editDoc = editDoc(_parameter);
+        updateConnection2Object(_parameter, editDoc);
 
+        final File file = createReport(_parameter, editDoc);
+        if (file != null) {
+            ret.put(ReturnValues.VALUES, file);
+            ret.put(ReturnValues.TRUE, true);
+        }
+        return ret;
+    }
+
+    @Override
+    public CIType getCIType()
+        throws EFapsException
+    {
+        return CISales.CollectionOrder;
+    }
 }
