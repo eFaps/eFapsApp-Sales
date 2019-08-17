162a1fb5ee9b0ce190d9007a00c7d67776ea914d
Jan Moxter <jan@moxter.net>
2019-08-16 19:33:47 -0500

    Update due to change in eql2

--- src/main/efaps/ESJP/org/efaps/esjp/sales/document/Template_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/document/Template_Base.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2003 - 2019 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.efaps.esjp.sales.document;
 
 import java.util.Collections;
@@ -17,9 +34,9 @@ import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.ci.CIType;
 import org.efaps.db.Insert;
 import org.efaps.db.Instance;
-import org.efaps.db.stmt.StmtFlag;
 import org.efaps.db.stmt.selection.Evaluator;
 import org.efaps.eql.EQL;
+import org.efaps.eql2.StmtFlag;
 import org.efaps.esjp.ci.CISales;
 import org.efaps.esjp.common.parameter.ParameterUtil;
 import org.efaps.esjp.erp.Currency;
