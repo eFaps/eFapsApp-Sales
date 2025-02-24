05d61334899e0fbc6f8a27115a4cf59357caa343
Jan Moxter <jan.moxter@moxter.net>
2025-02-24 18:43:01 -0500

    Correct esjp

--- src/main/efaps/ESJP/org/efaps/esjp/sales/graphql/IndividualProductDataFetcher.java
+++ src/main/efaps/ESJP/org/efaps/esjp/sales/graphql/IndividualProductDataFetcher.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.efaps.admin.program.esjp.EFapsApplication;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Instance;
